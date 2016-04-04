d8cce3d67fc0eaa4809e932470b7090d31968875
woenardi <jonathanmulyawan@gmail.com>
2016-04-04 15:52:49 +0800

    Repair error in migration

--- db/migrate/20160326175529_create_roles_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateRolesUsers < ActiveRecord::Migration
-	def change
-		create_table :roles_users, :id => false do |t|
-			t.references :role
-			t.references :user
-		add_index :roles_users, [:role_id, :user_id]
-		add_index :roles_users, :user_id
-	end
-end
