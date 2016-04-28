9023a60e30f7ea7816e4106a3f0115faec77e73c
woenardi <jonathanmulyawan@gmail.com>
2016-04-29 05:17:01 +0800

    remember me, server verification of user registration, repair errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
 
 	protect_from_forgery with: :exception
 	def current_user
-		if session[:user_id]
-			@current_user ||= User.find(session[:user_id]).username if session[:user_id]
+		if cookies[:auth_token]
+			@current_user ||= User.find_by_auth_token!(cookies[:auth_token])
 		else
 			nil
 		end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419165653) do
+ActiveRecord::Schema.define(version: 20160428202525) do
 
   create_table "contests", force: :cascade do |t|
     t.string   "name"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20160419165653) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "salt"
+    t.string   "auth_token"
   end
 
 end
