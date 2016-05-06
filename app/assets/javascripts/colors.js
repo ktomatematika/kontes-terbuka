98c2d5a27e72e2cdba1a198799b244c04d512698
donjar <7744han@gmail.com>
2016-05-07 01:32:27 +0800

    Closed #5 (users form), shade from Date.now(), FB box, responsive home

---
+++ app/assets/javascripts/colors.js
@@ -0,0 +1,29 @@
+//Place all the behaviors and hooks related to the matching controller here.
+//All this logic will automatically be available in application.js.
+//You can use CoffeeScript in this file: http://coffeescript.org/
+
+function choose_color() {
+	var num = Date.now() % 4;
+	var color = "";
+
+	if (num === 0) {
+		color = "red";
+	} else if (num === 1) {
+		color = "blue";
+	} else if (num === 2) {
+		color = "green";
+	} else if (num === 3) {
+		color = "yellow";
+	}
+
+	color += "-color";
+	return color;
+}
+
+function ready() {
+	var shade = choose_color();	
+	$('.has-shade').addClass(shade);
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
