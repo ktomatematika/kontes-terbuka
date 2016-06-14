5e6653ddcf38cc1108f783e0e42e37329ed599ed
Herbert Ilhan Tanujaya <tanujayah@garena.com>
2016-06-14 13:47:19 +0800

    Nuke Mulpin

--- config/routes.rb
+++ config/routes.rb
@@ -2,68 +2,66 @@ Rails.application.routes.draw do
 
 	mount RailsAdmin::Engine => '/penguasa', as: 'rails_admin'
 
-	scope '/mulpin' do
-		authenticated :user do
-			root to: 'home#index', as: :authenticated_root
-		end
-
-		resources :province do
-			resources :users
-		end
-
-		resources :status do
-			resources :users
-		end
-
-		resources :users
-		resources :roles
-		resources :contests do
-			get 'show_rules' => 'contests#show_rules', on: :member, as: :show_rules
-			post 'accept_rules' => 'contests#accept_rules', on: :collection, as: :accept_rules
-			resources :short_problems
-			resources :long_problems
-		end
-
-		resources :short_problems do
-			resources :short_submissions
-		end
-
-		resources :long_problems do
-			resources :long_submissions
-		end
-
-		resources :short_submissions
-		resources :long_submissions
-		resources :user_contests
-		root "welcome#index"
-
-		get '/sign' => 'welcome#sign'
-		get '/register' => 'users#new'
-
-		get '/login' => 'sessions#new'
-		post '/login' => 'sessions#create'
-		get '/logout' => 'sessions#destroy', as: :logout
-
-		get '/contests/:id/admin' => 'contests#admin', as: :contest_admin
-		get '/contests/:id/rules' => 'contests#rules'
-
-		get '/home/index' => 'home#index'
-		get '/faq' => 'home#faq'
-		get '/book' => 'home#book'
-		get '/donate' => 'home#donate'
-		get '/about' => 'home#about'
-		get '/sitemap' => 'home#sitemap'
-		get '/privacy' => 'home#privacy'
-		get '/terms' => 'home#terms'
-		get '/contact' => 'home#contact'
-
-		post '/short_problems/submit' => 'short_problems#submit'
-		post '/long_problems/submit' => 'long_problems#submit'
-
-		get '/magic' => 'home#send_magic_email', as: :magic
-
-		get '/home/admin' => 'home#admin'
+	authenticated :user do
+		root to: 'home#index', as: :authenticated_root
 	end
+
+	resources :province do
+		resources :users
+	end
+
+	resources :status do
+		resources :users
+	end
+
+	resources :users
+	resources :roles
+	resources :contests do
+		get 'show_rules' => 'contests#show_rules', on: :member, as: :show_rules
+		post 'accept_rules' => 'contests#accept_rules', on: :collection, as: :accept_rules
+		resources :short_problems
+		resources :long_problems
+	end
+
+	resources :short_problems do
+		resources :short_submissions
+	end
+
+	resources :long_problems do
+		resources :long_submissions
+	end
+
+	resources :short_submissions
+	resources :long_submissions
+	resources :user_contests
+	root "welcome#index"
+
+	get '/sign' => 'welcome#sign'
+	get '/register' => 'users#new'
+
+	get '/login' => 'sessions#new'
+	post '/login' => 'sessions#create'
+	get '/logout' => 'sessions#destroy', as: :logout
+
+	get '/contests/:id/admin' => 'contests#admin', as: :contest_admin
+	get '/contests/:id/rules' => 'contests#rules'
+
+	get '/home/index' => 'home#index'
+	get '/faq' => 'home#faq'
+	get '/book' => 'home#book'
+	get '/donate' => 'home#donate'
+	get '/about' => 'home#about'
+	get '/sitemap' => 'home#sitemap'
+	get '/privacy' => 'home#privacy'
+	get '/terms' => 'home#terms'
+	get '/contact' => 'home#contact'
+
+	post '/short_problems/submit' => 'short_problems#submit'
+	post '/long_problems/submit' => 'long_problems#submit'
+
+	get '/magic' => 'home#send_magic_email', as: :magic
+
+	get '/home/admin' => 'home#admin'
 
 	post '/check' => 'users#check_unique'
 end
