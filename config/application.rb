9629b2c1c784ee447cab37b67908371eaf5034f7
woenardi <jonathanmulyawan@gmail.com>
2016-06-19 17:37:57 +0800

    config

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../boot', __FILE__)
+	
 
 require 'rails/all'
 require 'rack/protection'
@@ -15,7 +16,6 @@ module KontesTerbuka
 
 		# Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
 		# Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-		# config.time_zone = 'Central Time (US & Canada)'
 
 		# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
 		# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -25,5 +25,7 @@ module KontesTerbuka
 		config.active_record.raise_in_transactional_callbacks = true
 
 		config.middleware.use Rack::Protection
+
+		config.web_console.whitelisted_ips = '10.0.2.2'
 	end
 end
