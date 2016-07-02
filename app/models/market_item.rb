e10f058113df4041268cb93341c69414ecb36d11
woenardi <jonathanmulyawan@gmail.com>
2016-07-02 18:19:23 +0800

    market item model

---
+++ app/models/market_item.rb
@@ -0,0 +1,9 @@
+class MarketItem < ActiveRecord::Base
+
+
+  has_attached_file :picture,
+                  url: '/market/pictures/:id/:basename.:extension',
+                  path: ':rails_root/public/market/pictures/:id/:basename.:extension'
+
+  validates_attachment_content_type :picture, content_type: /image/
+end
