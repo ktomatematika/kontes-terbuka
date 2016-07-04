16d2109690e122718942ec3518f2edd3f9aca23d
Herbert Ilhan Tanujaya <tanujayah@garena.com>
2016-07-04 14:23:43 +0800

    Contest helper --> remove reference to num_of_s/l_qs

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -14,8 +14,8 @@ module ContestsHelper
         name: c.name,
         start_time: c.start_time,
         end_time: c.end_time,
-        number_of_short_questions: c.number_of_short_questions,
-        number_of_long_questions: c.number_of_long_questions,
+        number_of_short_questions: ShortProblem.where(contest: c).length,
+        number_of_long_questions: LongProblem.where(contest: c).length,
         path: contest_path(c)
       }
     end
