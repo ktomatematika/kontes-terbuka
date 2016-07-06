742694a39958de73ee345f6a6a3c94f5ed593a7d
donjar <7744han@gmail.com>
2016-07-06 21:30:45 +0800

    change model

--- app/models/market_item.rb
+++ app/models/market_item.rb
@@ -1,8 +1,4 @@
 class MarketItem < ActiveRecord::Base
   has_paper_trail
-  has_attached_file :picture,
-                    url: '/market/pictures/:id/:basename.:extension',
-                    path: ':rails_root/public/market/pictures/:id/:basename.:extension'
-
-  validates_attachment_content_type :picture, content_type: /image/
+  has_many :market_item_pictures
 end
