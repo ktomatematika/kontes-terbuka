316c62ed70f2a3fbad00ea8af83abf5884eb85ed
Herbert Ilhan Tanujaya <tanujayah@garena.com>
2016-07-12 18:58:38 +0800

    Email notifs modified a bit

--- app/tasks/email_notifications.rb
+++ app/tasks/email_notifications.rb
@@ -1,3 +1,12 @@
-class EmailNotifications
+module EmailNotifications
+  attr_accessor :time_range
 
+  def contest_starting(contest, time_text)
+    subject = "Kontes dimulai dalam #{time_text}"
+    text = "Hanya mengingatkan saja, #{contest} akan dimulai #{time_text} " \
+      'kemudian. Siapkan segala peralatan dan alat perang Anda!'
+    Mailgun.send_message contest: contest, text: text, subject: subject,
+                         bcc_array: User.pluck(:email)
+  end
+  handle_asynchronously :contest_starting, run_at: proc { time_range }
 end
