8b9f65ac78131af42bb6ca3046ad6f49879cbf29
Herbert Ilhan Tanujaya <tanujayah@garena.com>
2016-07-13 17:11:09 +0800

    FAQ done

---
+++ app/assets/javascripts/faq.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	for (var i = 0; i < faq_data.length; i++) {
+		var data = faq_data[i];
+		$('#faq-entries').append(
+				'<div class="faq question">' + data.question + '</div>' +
+				'<div class="faq answer">' + data.answer + '</div>');
+	}
+
+	$('.answer').hide();
+	$('.question').click(function() {
+		var answer = $(this).next();
+		var deactivate = (answer.css('display') !== 'none')
+		$('.answer').slideUp('fast');
+		if (!deactivate) {
+			$(this).next().slideDown('fast');
+		}
+	});
+});
---
+++ app/assets/javascripts/faq_entries.js
@@ -0,0 +1,38 @@
+faq_data = [
+{
+	question: 'Cara daftarnya bagaimana, kak?',
+	answer: 'Cukup buat akun di ktom.tomi.or.id (website ini). Jika lombanya sudah dimulai Anda bisa langsung masuk ke page lombanya dan mulai mengerjakan soal-soalnya. Jika belum, Anda akan dikirimkan email 1 hari sebelum kontes dimulai dan ketika kontes dimulai, sebagai pengingat.',
+},
+{
+	question: 'Bayarnya gimana kak?',
+	answer: 'Bayar? Kontes ini sepenuhnya gratis kok! Cius 100% selamanya :D',
+},
+{
+	question: 'Hadiahnya apa kak?',
+	answer: 'Dengan mengikuti kontes ini dengan sungguh-sungguh Anda bisa mendapatkan penghargaan emas, perak, ataupun perunggu. Nanti Anda akan mendapatkan poin yang jumlahnya ditentukan dari berbagai kriteria. Poinnya bisa ditukarkan dengan berbagai barang menarik loh dengan gratis! Anda juga akan mendapatkan sertifikat atas apresiasi Anda. <br>Namun, menurut kami hadiah yang paling berguna adalah ilmu yang Anda bisa dapatkan dengan latihan rutin di kontes-kontes ini! Kerja keras dan keuletan Anda akan terbayar dengan prestasi-prestasi yang akan Anda ukir nanti. Siapa tahu Anda bisa mengikuti lomba internasional. A thousand miles start from a single step. :)',
+},
+{
+	question: 'Murid SD/murid SMP/murid SMA/guru/dosen/mahasiswa/kakek/nenek/bayi boleh ikut tidak?',
+	answer: 'Dengan senang hati! Biarpun kontes-kontes kami pada umumnya setingkat OSN SMA tingkat kabupaten/provinsi, siapa saja boleh ikut, baik itu untuk latihan maupun untuk menikmati soal-soal kami. Namanya saja kontes terbuka :)',
+},
+{
+	question: 'Wah kok keren banget kak?',
+	answer: 'Iya dong, makanya ayok daftar :)',
+},
+{
+	question: 'Solusinya bisa dicek di mana?',
+	answer: 'Kami memang tidak mengeluarkan kunci jawaban, solusi, ataupun skema penilaian kami. Namun, Anda bisa mendiskusikan soal-soalnya di olimpiade.org. Tim kami sudah siaga di sana menjawab pertanyaan-pertanyaan Anda. Anda juga bisa saling diskusi dan mengenal satu sama lain. Kami percaya format seperti ini akan membuat Anda jauh lebih berkembang daripada sekadar melihat solusinya saja. Namun, kami juga mengeluarkan buku KTO Matematika yang berisi kumpulan soal dan solusi kami! Untuk info lebih lanjut, bisa dilihat jawaban di bawah ini :)',
+},
+{
+	question: 'Dengar-dengar ada bukunya juga ya?',
+	answer: 'Benar sekali. Kami mencetak buku ini 6 bulan sekali; buku ini berisi kumpulan soal dan solusi kontes-kontes kami. Ada berbagai tips, trik, dan komentar juga dari tim kami yang sudah sangat berpengalaman. Banyak juga referensi lanjutan untuk mempelajari suatu hal lebih lanjut. Info selebihnya bisa dicek di link ini.',
+},
+{
+	question: 'Kak kalau mau donasi bagaimana?',
+	answer: 'Wah, Anda baik sekali :)',
+},
+{
+	question: 'Sertifikatnya di mana kak?',
+	answer: 'Untuk mendapatkan sertifikat, Anda perlu mengisi formulir feedback ke kami. Anda bisa mengisi berbagai kritik dan saran yang Anda punyai ke kami untuk kami tinjau, agar kami bisa berkembang.',
+},
+]
