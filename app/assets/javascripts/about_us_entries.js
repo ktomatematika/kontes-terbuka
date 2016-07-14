93d3f7aa40d76112b4ce0d6296a6aa3aea6c91aa
Herbert Ilhan Tanujaya <tanujayah@garena.com>
2016-07-14 16:00:36 +0800

    + Ricky, Cis

--- app/assets/javascripts/about_us_entries.js
+++ app/assets/javascripts/about_us_entries.js
@@ -5,4 +5,14 @@ var about_us_data = [
 		description: 'Herbert Ilhan Tanujaya telah diberi kehormatan untuk mewakili tanah air tercintanya di IMO dan mendapatkan 2 medali perunggu di tahun 2014 dan 2015. Walaupun saat ini dia melanjutkan studinya di NUS, Singapura bidang Teknik Komputer, dia menunjukkan dedikasinya yang tinggi ke olimpiade matematika Indonesia dengan menjadi ketua umum KTO Matematika. Dia juga merupakan salah satu developer utama website yang sedang Anda kunjungi ini. Dia juga sangat gemar bermain game musik, dan suka menghabiskan waktunya bermain di game center berbagai mal.',
 		image: 'ilhan.jpg',
 	},
+	{
+		name: 'Ricky The Ising',
+		description: 'Berasal dari Sulawesi Selatan, Ricky The Ising adalah salah satu dari sedikit orang yang berada di luar pulau Jawa namun bisa meraih medali emas di OSN. Pemenang medali perak OSN 2014 dan medali emas OSN 2015 ini menjadi mahasiswa baru di UGM, Yogyakarta, jurusan Matematika. Dia meluangkan banyak waktunya untuk membantu KTO Matematika di bagian soal dan dalam pembuatan konten di OA LINE KTOM. Kemampuannya dalam bermain Dota 2 juga tidak kalah dengan kemampuan matematikanya; MMR dia di Dota mencapai 8000.'
+		image: 'ricky.jpg',
+	},
+	{
+		name: 'Fransisca Andriani',
+		description: 'Fransisca Andriani (Cis) pernah beruntung mendapatkan medali perak OSN 2013 dan medali emas OSN 2015 meskipun kemampuannya tidak seberapa. Dia juga mendapatkan keberuntungan lagi di 2016 saat diterima di Nanyang Technological University, Singapura untuk melanjutkan studi di bidang Matematika. Peran Cis di KTOM selain sebagai pengisi kuota perempuan (karena tidak ada perempuan lain yang mau) juga sebagai designer, pengelola utama Official Account LINE KTOM, dan terkadang korektor jawaban kontes. Dia juga sangat suka menyanyi, membaca, dan pandai menggerak2kan anggota wajah seperti hidung telinga dan alis.'
+		image: 'cis.jpg',
+	},
 ];
