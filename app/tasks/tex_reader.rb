97aea16528e7ef3ffc492abedd024e1ab8c29cb2
donjar <7744han@gmail.com>
2016-07-21 23:44:52 +0800

    aaaaa

--- app/tasks/tex_reader.rb
+++ app/tasks/tex_reader.rb
@@ -4,21 +4,21 @@ class TexReader
   LP_START_SEPARATOR = '%%% START Bagian B'.freeze
   LP_END_SEPARATOR = '%%% END Bagian B'.freeze
 
-  attr_accessor :tex_file
+  attr_accessor :contest
   attr_accessor :answers
   def initialize(ctst, answers)
-    @tex_file = Paperclip.io_adapters.for(ctst.problem_tex).read
+    @contest = ctst
     @answers = answers
   end
 
   def run
     sp_process.each_with_index do |sp, index|
-      ShortProblem.create(contest: contest, problem_no: (index + 1),
+      ShortProblem.create(contest: @contest, problem_no: (index + 1),
                           statement: sp, answer: answers[index])
     end
 
     lp_process.each_with_index do |lp, index|
-      LongProblem.create(contest: contest, problem_no: (index + 1),
+      LongProblem.create(contest: @contest, problem_no: (index + 1),
                          statement: lp)
     end
   end
@@ -26,19 +26,21 @@ class TexReader
   private
 
   def sp_process
-    sp_start_index = @tex_file.index(SP_START_SEPARATOR) +
+    tex_file = Paperclip.io_adapters.for(ctst.problem_tex).read
+    sp_start_index = tex_file.index(SP_START_SEPARATOR) +
                      SP_START_SEPARATOR.length
-    sp_end_index = @tex_file.index(SP_END_SEPARATOR)
+    sp_end_index = tex_file.index(SP_END_SEPARATOR)
 
-    tex_file_process @tex_file[sp_start_index...sp_end_index]
+    tex_file_process tex_file[sp_start_index...sp_end_index]
   end
 
   def lp_process
-    lp_start_index = @tex_file.index(LP_START_SEPARATOR) +
+    tex_file = Paperclip.io_adapters.for(ctst.problem_tex).read
+    lp_start_index = tex_file.index(LP_START_SEPARATOR) +
                      LP_START_SEPARATOR.length
-    lp_end_index = @tex_file.index(LP_END_SEPARATOR)
+    lp_end_index = tex_file.index(LP_END_SEPARATOR)
 
-    tex_file_process @tex_file[lp_start_index...lp_end_index]
+    tex_file_process tex_file[lp_start_index...lp_end_index]
   end
 
   def tex_file_process(tex_string)
