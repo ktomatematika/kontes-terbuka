8dbb0a107931ea1792d431881d80c2fd7aa694f0
Herbert Ilhan Tanujaya <tanujayah@garena.com>
2016-07-22 15:08:41 +0800

    Validation on long submissions

--- app/controllers/long_submissions_controller.rb
+++ app/controllers/long_submissions_controller.rb
@@ -1,12 +1,20 @@
 class LongSubmissionsController < ApplicationController
   def submit
-    LongSubmission.transaction do
-      if LongSubmission.find(params[:id]).update(submission_params)
-        flash[:notice] = 'Jawaban bagian B berhasil diupload!'
-      else
-        flash[:alert] = 'Jawaban bagian B gagal diupload! Pastikan nomor
-        halaman di setiap soal berbeda-beda dan file-file yang Anda upload
-        merupakan file PDF/zip/Word/gambar.'
+    if params[:long_submission].nil?
+      flash[:alert] = 'Anda tidak mengupload apapun!'
+    elsif params[:long_submission][:submission_pages_attributes].map do |_, v|
+      v['page_number']
+    end.include? ''
+      flash[:alert] = 'Isi nomor halaman di setiap file yang Anda upload.'
+    else
+      LongSubmission.transaction do
+        if LongSubmission.find(params[:id]).update(submission_params)
+          flash[:notice] = 'Jawaban bagian B berhasil diupload!'
+        else
+          flash[:alert] = 'Jawaban bagian B gagal diupload! Pastikan nomor
+          halaman di setiap soal berbeda-beda dan file-file yang Anda upload
+          merupakan file PDF/zip/Word/gambar.'
+        end
       end
     end
 
