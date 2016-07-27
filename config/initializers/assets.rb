931d9b8c2a2ec9fa6da80f342e743bf1244bfbbf
donjar <7744han@gmail.com>
2016-07-27 22:28:40 +0800

    remove public/assets, try error codes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.paths <<
 
 Rails.application.config.assets.register_mime_type('text/html', '.html')
 
-NonStupidDigestAssets.whitelist = [/.html$/]
+NonStupidDigestAssets.whitelist = [/.html/]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder
