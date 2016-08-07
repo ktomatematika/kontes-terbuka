bd464541afadd08db62dffb1ccc7e6f271123302
donjar <7744han@gmail.com>
2016-08-08 06:23:19 +0800

    Try greeting

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -10,11 +10,24 @@ class LineController < ApplicationController
     receive_req = Line::Bot::Receive::Request.new request.env
 
     receive_req.data.each do |msg|
+      mid = msg.from_mid
+      profile = LineClient.get_user_profile(mid).contacts.first.display_name
+
       case msg.content
       when Line::Bot::Message::Text
-        LineClient.send_text to_mid: msg.from_mid, text: msg.content.to_s
+        text = msg.content[:text]
+        match = (/^Selamat (.*), KTOM/.match text)
+        if !match.nil?
+          response = "Selamat #{match[1]}, #{profile}! MID kamu #{mid}."
+        elsif text == 'Afif bodoh'
+          response = 'Memang bego'
+        else
+          response = 'Halo! Saya mau asal aja.'
+        end
+
+        LineClient.send_text to_mid: mid, text: response
       when Line::Bot::Operation::AddedAsFriend
-        LineClient.send_text to_mid: msg.from_mid, text: msg.content.to_s
+        LineClient.send_text to_mid: mid, text: 'Halo!'
       end
     end
 
