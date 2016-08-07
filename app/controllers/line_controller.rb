87b9aea3a32468f661b469f5a8d37ebb92acd6b6
donjar <7744han@gmail.com>
2016-08-08 05:32:19 +0800

    Test bounce messages

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -1,6 +1,20 @@
 class LineController < ApplicationController
+  skip_before_action :verify_authenticity_token, :require_login
+
   def callback
-    Mailgun.send_message to: '7744han@gmail.com', text: params.to_s
+    signature = request.headers['HTTP_X_LINE_CHANNELSIGNATURE']
+    unless LineClient.validate_signature(request.raw_post, signature)
+      head :bad_request
+    end
+
+    receive_req = Line::Bot::Receive::Request.new params
+
+    receive_req.data.each do |msg|
+      if msg.content == Line::Bot::Message::Text
+        LineClient.send_text to: msg.from_mid, text: msg.content[:text]
+      end
+    end
+
     head :ok
   end
 end
