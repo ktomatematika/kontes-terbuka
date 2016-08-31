d7abdc2d7c39cb0d16f9366476241afb030e2da5
woenardi <jonathanmulyawan@gmail.com>
2016-08-31 18:46:45 +0800

    move password and verification from User model to separate concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@
 #
 
 class User < ActiveRecord::Base
-  include Rails.application.routes.url_helpers
+  include UserPasswordVerification
   rolify
   has_paper_trail
   enforce_migration_validations
@@ -109,90 +109,8 @@ class User < ActiveRecord::Base
     PointTransaction.where(user: self).sum(:point)
   end
 
-  # TODO: Refactor several of the methods to concerns.
-  # Password and verification concerns.
-
-  MAX_TRIES = 10
-
-  def encrypt_password
-    self.salt = BCrypt::Engine.generate_salt
-    self.hashed_password = BCrypt::Engine.hash_secret(password, salt)
-  end
-
   def self.get_user(username_or_email)
     User.find_by(username: username_or_email) ||
       User.find_by(email: username_or_email)
-  end
-
-  def authenticate(password)
-    hashed_password == BCrypt::Engine.hash_secret(password, salt)
-  end
-
-  def clear_password
-    self.password = nil
-  end
-
-  def generate_token(column)
-    loop do
-      self[column] = SecureRandom.urlsafe_base64
-      break unless User.exists?(column => self[column])
-    end
-  end
-
-  def reset_password
-    generate_token(:verification)
-    save
-    text = 'Untuk melanjutkan process reset password user Anda, klik link ' \
-      "ini: \n\n #{reset_password_url verification: verification}"
-    Mailgun.send_message to: user.email,
-                         subject: 'Reset Password KTO Matematika',
-                         text: text
-  end
-
-  VERIFY_TIME = 4.hours
-  VERIFY_TIME_INDO = '4 jam'.freeze
-  def destroy_if_unverified
-    unless enabled
-      Ajat.warn "verification_expiry|uname:#{username}"
-      destroy
-    end
-  end
-  handle_asynchronously :destroy_if_unverified,
-                        run_at: proc { VERIFY_TIME.from_now },
-                        queue: 'destroy_if_unverified'
-
-  def send_verify_email
-    link = verify_url verification: verification
-    text = 'User berhasil dibuat! Sekarang, buka link ini untuk ' \
-      "memverifikasikan email Anda:\n\n#{link}\n\n" \
-      "Anda hanya memiliki waktu #{VERIFY_TIME_INDO} untuk mengverifikasi. " \
-      'Jika Anda tidak mendaftar ke Kontes Terbuka Olimpiade Matematika, ' \
-      'Anda boleh mengacuhkan email ini.'
-    Mailgun.send_message to: email,
-                         subject: 'Konfirmasi Pendaftaran Kontes ' \
-                                  'Terbuka Olimpiade Matematika',
-                         text: text
-    destroy_if_unverified
-  end
-  handle_asynchronously :send_verify_email, queue: 'send_verify_email'
-
-  def enable
-    update(enabled: true, verification: nil)
-    Notification.find_each do |n|
-      UserNotification.create(user: self, notification: n)
-    end
-  end
-
-  def forgot_password_process
-    generate_token(:verification)
-    save
-    link = reset_password_url verification: verification
-    Mailgun.send_message to: email,
-                         subject: 'Reset Password KTO Matematika',
-                         text: 'Klik link ini untuk mengreset password ' \
-                         "Anda:\n#{link}\n\n" \
-                         'Jika Anda tidak meminta password Anda untuk ' \
-                         'direset, acuhkan saja email ini.'
-    Ajat.info "forgot_password_email_sent|uid:#{id}"
   end
 end
