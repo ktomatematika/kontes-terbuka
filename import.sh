b81b75be6fa450c640bb93efbd65f197bda9f71f
donjar <7744han@gmail.com>
2016-09-14 20:38:43 +0800

    more changes in gems + script

--- import.sh
+++ import.sh
@@ -1,5 +1,3 @@
 bin/rake db:drop db:create
-sudo -u postgres psql -c "alter role ubuntu superuser;"
-
 cd import
-cat * | psql --set ON_ERROR_STOP=on -1 kontes_terbuka
+cat * | sudo -u postgres psql --set ON_ERROR_STOP=on -1 kontes_terbuka
