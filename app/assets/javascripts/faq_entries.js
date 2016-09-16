5cc7e3dfa4f48ee5a64b1ffd61ea807a60112619
donjar <7744han@gmail.com>
2016-09-16 10:36:29 +0800

    add more faq

--- app/assets/javascripts/faq_entries.js
+++ app/assets/javascripts/faq_entries.js
@@ -1,39 +1,71 @@
 /* exported faq_data */
 var faq_data = [
 	{
+		question: 'KTOM itu apa ya?',
+		answer: 'KTOM, singkatan dari Kontes Terbuka Olimpiade Matematika, adalah sebuah inisiatif dari Tim Olimpiade Matematika Indonesia untuk menyediakan sarana latihan olimpiade matematika untuk semua. Cek <a href="ktom.tomi.or.id/about">link ini</a> kalau mau tau lebih lanjut!',
+	},
+	{
 		question: 'Cara daftarnya bagaimana, kak?',
-		answer: 'Cukup buat akun di ktom.tomi.or.id (website ini). Jika lombanya sudah dimulai Anda bisa langsung masuk ke page lombanya dan mulai mengerjakan soal-soalnya. Jika belum, Anda akan dikirimkan email 1 hari sebelum kontes dimulai dan ketika kontes dimulai, sebagai pengingat.',
+		answer: 'Cukup buat akun di ktom.tomi.or.id (website ini). Jika lombanya sudah dimulai, Anda bisa langsung masuk ke page lombanya dan mulai mengerjakan soal-soalnya. Jika belum, Anda akan dikirimkan email 1 hari sebelum kontes dimulai dan ketika kontes dimulai, sebagai pengingat.',
 	},
 	{
 		question: 'Bayarnya gimana kak?',
 		answer: 'Bayar? Kontes ini sepenuhnya gratis kok! Cius 100% selamanya :D',
 	},
 	{
-		question: 'Hadiahnya apa kak?',
-		answer: '<p>Dengan mengikuti kontes ini dengan sungguh-sungguh, Anda bisa mendapatkan penghargaan emas, perak, ataupun perunggu. Nanti Anda akan mendapatkan kubus yang jumlahnya ditentukan dari berbagai kriteria. Kubusnya bisa ditukarkan dengan berbagai barang menarik loh dengan gratis! Anda juga akan mendapatkan sertifikat atas apresiasi Anda.</p><p>Namun, menurut kami hadiah yang paling berguna adalah ilmu yang Anda bisa dapatkan dengan latihan rutin di kontes-kontes ini! Kerja keras dan keuletan Anda akan terbayar dengan prestasi-prestasi yang akan Anda ukir nanti. Siapa tahu Anda bisa mengikuti lomba internasional. A thousand miles start from a single step. :)</p>',
+		question: 'Aku ikut KTOM ini keuntungannya apa kak?',
+		answer: '<p>Dengan mengikuti kontes ini dengan sungguh-sungguh, Anda bisa mendapatkan penghargaan emas, perak, ataupun perunggu. Nanti Anda akan mendapatkan kubus yang jumlahnya ditentukan dari berbagai kriteria. Kubusnya bisa ditukarkan dengan berbagai barang menarik loh dengan gratis! Anda juga akan mendapatkan sertifikat atas apresiasi Anda.</p><p>Namun, menurut kami hadiah yang paling berguna adalah ilmu yang Anda bisa dapatkan dengan latihan rutin di kontes-kontes ini! Kami memberikan umpan balik atas semua solusi esai/uraian yang telah Anda kerjakan, sehingga Anda bisa langsung belajar dari para ahlinya dan membenahi diri dari kesalahn. Kerja keras dan keuletan Anda akan terbayar dengan prestasi-prestasi yang akan Anda ukir nanti. Siapa tahu Anda bisa mengikuti lomba internasional. A thousand miles start from a single step. :)</p>',
+	},
+	{
+		question: 'Nanti lombanya kayak gimana kak?',
+		answer: 'Sebelum lombanya dimulai, kamu akan dikirimkan email sebagai pengingat. Ketika lombanya dimulai kamu cukup buka website ini saja! Nanti kamu bisa langsung download soal dalam pdf dan menginput jawaban langsung di website. Untuk bagian esai, solusi boleh diketik, difoto, ataupun discan. Kamu bisa langsung upload jawabannya di website kami juga! Tidak perlu repot :)',
+	},
+	{
+		question: 'Ini hanya untuk orang-orang di Indonesia saja kah?',
+		answer: 'Sebenarnya kami memperbolehkan siapapun untuk ikut. Namun, pada saat ini, kami hanya menyediakan soal-soal berbahasa Indonesia. Tunggu perkembangan kami selanjutnya; kami juga ingin <i>go international</i>!',
 	},
 	{
 		question: 'Murid SD/murid SMP/murid SMA/guru/dosen/mahasiswa/kakek/nenek/bayi boleh ikut tidak?',
 		answer: 'Dengan senang hati! Biarpun kontes-kontes kami pada umumnya setingkat OSN SMA tingkat kabupaten/provinsi, siapa saja boleh ikut, baik itu untuk latihan maupun untuk menikmati soal-soal kami. Namanya saja kontes terbuka :)',
 	},
 	{
+		question: 'Tingkat kesulitan soalnya bagaimana?',
+		answer: 'Kami memberikan soal-soal yang tingkat kesulitannya bervariasi, dari soal-soal OSN SMA tingkat kabupaten yang lebih mudah hingga OSN SMA tingkat nasional. Hal ini kami lakukan agar setiap orang, tidak peduli kemampuannya, bisa berpartisipasi dan mengasah kemampuannya.',
+	},
+	{
 		question: 'Wah kok keren banget kak?',
-		answer: 'Iya dong, makanya ayok daftar :)',
+		answer: 'Iya dong, makanya ayok <a href="ktom.tomi.or.id/register">daftar</a> :)',
+	},
+	{
+		question: 'Cara belajarnya bagaimana kak?',
+		answer: 'Ada banyak kok sarananya! Untuk soal-soal kontes sebelumnya, Anda bisa cek di halaman utama kami. Anda bisa mulai berlatih dari sumber itu! Kami juga memiliki official account LINE yang memberikan kuis matematika mingguan! Selain itu, kami juga memiliki buku kumpulan soal dan solusi KTO Matematika yang bisa dicek <a href="ktom.tomi.or.id/book">di sini.</a> Jangan patah semangat! Kamu pasti bisa!',
+	},
+	{
+		question: 'Aduh, solusi aku tidak bisa ter-upload!',
+		answer: 'Pertama, pastikan bahwa total file yang Anda upload tidak melebihi 10 MB di setiap soal. Jika masih error, kemungkinan ada masalah dengan server kami :(. Tunggu pengumuman selanjutnya ya! Anda juga bisa <a href="ktom.tomi.or.id/contact">mengontak kami.</a>',
 	},
 	{
 		question: 'Solusinya bisa dicek di mana?',
-		answer: 'Kami memang tidak mengeluarkan kunci jawaban, solusi, ataupun skema penilaian kami. Namun, Anda bisa mendiskusikan soal-soalnya di olimpiade.org. Tim kami sudah siaga di sana menjawab pertanyaan-pertanyaan Anda. Anda juga bisa saling diskusi dan mengenal satu sama lain. Kami percaya format seperti ini akan membuat Anda jauh lebih berkembang daripada sekadar melihat solusinya saja. Namun, kami juga mengeluarkan buku KTO Matematika yang berisi kumpulan soal dan solusi kami! Untuk info lebih lanjut, bisa dilihat jawaban di bawah ini :)',
+		answer: 'Kami memang tidak mengeluarkan kunci jawaban, solusi, ataupun skema penilaian kami. Namun, Anda bisa mendiskusikan soal-soalnya di berbagai media yang sudah kami sediakan. Tim kami sudah siaga di sana menjawab pertanyaan-pertanyaan Anda. Anda juga bisa saling diskusi dan mengenal satu sama lain. Kami percaya format seperti ini akan membuat Anda jauh lebih berkembang daripada sekadar melihat solusinya saja. Namun, kami juga mengeluarkan buku KTO Matematika yang berisi kumpulan soal dan solusi kami! Untuk info lebih lanjut, bisa dilihat jawaban di bawah ini :)',
 	},
 	{
 		question: 'Dengar-dengar ada bukunya juga ya?',
-		answer: 'Benar sekali. Kami mencetak buku ini 6 bulan sekali; buku ini berisi kumpulan soal dan solusi kontes-kontes kami. Ada berbagai tips, trik, dan komentar juga dari tim kami yang sudah sangat berpengalaman. Banyak juga referensi lanjutan untuk mempelajari suatu hal lebih lanjut. Info selebihnya bisa dicek di link ini.',
+		answer: 'Benar sekali. Kami mencetak buku ini 6 bulan sekali; buku ini berisi kumpulan soal dan solusi kontes-kontes kami. Ada berbagai tips, trik, dan komentar juga dari tim kami yang sudah sangat berpengalaman. Banyak juga referensi lanjutan untuk mempelajari suatu hal lebih lanjut. Info selebihnya bisa dicek di <a href="ktom.tomi.or.id/book">link ini.</a>',
 	},
 	{
 		question: 'Kak kalau mau donasi bagaimana?',
-		answer: 'Wah, Anda baik sekali :) Jika Anda berminat memberi donasi, baik secara materi ataupun nonmateri, silakan melihat ktom.tomi.or.id/donate atau mengontak kami di ktom.tomi.or.id/contact :)',
+		answer: 'Wah, Anda baik sekali :) Jika Anda berminat memberi donasi, baik secara materi ataupun nonmateri, silakan cek <a href="ktom.tomi.or.id/donate">link ini</a> atau kontak kami <a href="ktom.tomi.or.id/contact">di sini</a> :)',
 	},
 	{
 		question: 'Sertifikatnya di mana kak?',
-		answer: 'Untuk mendapatkan sertifikat, Anda perlu mengisi formulir feedback ke kami. Anda bisa mengisi berbagai kritik dan saran yang Anda punyai ke kami untuk kami tinjau, agar kami bisa berkembang.',
+		answer: 'Untuk mendapatkan sertifikat, Anda perlu mengisi formulir feedback ke kami. Anda bisa mengisi berbagai kritik dan saran yang Anda punyai ke kami untuk kami tinjau, agar kami bisa berkembang. Selain itu, Anda perlu mendapatkan nilai setidaknya 1 poin di kontes yang bersangkutan.',
+	},
+	{
+		question: 'Kubus itu apa kak?',
+		answer: 'Kubus adalah sistem penghargaan kami. Dengan berpartisipasi, kamu bisa mengumpulkan kubus-kubus ini untuk ditukarkan nantinya dengan barang-barang yang keren, loh! Pasar untuk menukar kubus ini akan muncul dalam waktu dekat!',
+	},
+	{
+		question: 'Bagaimana cara mendapatkan kubus-kubus ini?',
+		answer: 'Salah satu cara yang termudah adalah dengan berpartisipasi di kontes. Berikut adalah kriteria pembagian kubus saat ini (dapat berubah sewaktu-waktu tanpa pemberitahuan): <ul><li>Penghargaan: emas mendapatkan 5 kubus, perak mendapatkan 4 kubus, perunggu mendapatkan 3 kubus</li><li>Solusi Bagian B: 1 poin untuk setiap solusi yang memiliki ide di dalamnya (tidak bernilai "-"), dan 1 poin tambahan untuk setiap solusi yang mendapatkan nilai 7.</li></ul> Anda juga bisa mendapatkan kubus dengan berbagai cara yang lain; tunggu perkembangan selanjutnya dari kami! :)',
 	},
 ];
