bbcdcf6e3b9be98822c4827a636fba057eed7b8d
donjar <7744han@gmail.com>
2016-09-17 09:23:15 +0800

    check for #iklan-lomba

--- app/assets/javascripts/iklan_lomba.js
+++ app/assets/javascripts/iklan_lomba.js
@@ -3,7 +3,9 @@ var iklan_desc = 'Tunggu kerjasama kami dengan berbagai olimpiade ' +
 'matematika di Indonesia yang berikutnya!';
 
 $(document).ready(function() {
-	$('#judul-iklan')[0].innerHTML = iklan_title;
-	$('#desc-iklan')[0].innerHTML = iklan_desc;
-	$('#iklan-lomba img').prop('alt', iklan_title);
+	if ($('#iklan-lomba').length !== 0) {
+		$('#judul-iklan')[0].innerHTML = iklan_title;
+		$('#desc-iklan')[0].innerHTML = iklan_desc;
+		$('#iklan-lomba img').prop('alt', iklan_title);
+	}
 });
