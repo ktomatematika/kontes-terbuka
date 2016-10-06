a70b3f9415ed67c462ad9ee7de789b9db9ba4ace
woenardi <jonathanmulyawan@gmail.com>
2016-10-07 03:45:31 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006012803) do
+ActiveRecord::Schema.define(version: 20161006192548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,17 @@ ActiveRecord::Schema.define(version: 20161006012803) do
     t.integer  "price",       :null=>false
     t.datetime "created_at",  :null=>false
     t.datetime "updated_at",  :null=>false
+    t.integer  "quantity"
+  end
+
+  create_table "market_orders", force: :cascade do |t|
+    t.integer  "user_id",        :index=>{:name=>"index_market_orders_on_user_id", :using=>:btree}
+    t.integer  "market_item_id", :index=>{:name=>"index_market_orders_on_market_item_id", :using=>:btree}
+    t.integer  "quantity"
+    t.text     "memo"
+    t.integer  "status"
+    t.datetime "created_at",     :null=>false
+    t.datetime "updated_at",     :null=>false
   end
 
   create_table "migration_validators", force: :cascade do |t|
