7fdfbe3914b4f51bd522da7a22cccf94d924a3ee
donjar <7744han@gmail.com>
2016-10-21 22:19:38 +0800

    javascript changes on no next contest

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,38 +4,49 @@ function fill_home_text() {
 
 	var next_important_contest = $('#home-contest-data');
 	if (next_important_contest.length !== 0) {
-		var end = erb_to_date(next_important_contest.data('end-time'));
-		var results = erb_to_date(next_important_contest.data('result-time'));
-		var results_released = next_important_contest.data('results-released');
-		$('#next-contest-name').text(next_important_contest.data('name'));
-		var subtitle_text, button_text;
+		var title_text, subtitle_text, button_text;
 
-		if (results_released) {
-			button_text = 'Hasil sudah diumumkan!';
-		} else if (current > results) {
-			subtitle_text = ' Dikarenakan berbagai halangan, hasil kontes' +
-				' belum keluar. Mohon maaf atas ketidaknyamannya dan' +
-				' mohon bersabar :(';
+		if (next_important_contest.data('name') === null) {
+			title_text = 'Tunggu kontes kami selanjutnya!';
+			subtitle_text = 'Kami sedang mempersiapkan banyak hal di ' +
+				'balik tirai!';
 			$('#home-btn-daftar').hide();
-		} else if (current > end) {
-			// Show time to results
-			subtitle_text = 'Hasil diumumkan paling telat ' +
-					results.format_indo();
-			button_text = current.indo_go_to(results);
 		} else {
-			// Show time to next contest start
-			var start = erb_to_date(next_important_contest.data('start-time'));
-			subtitle_text = start.format_indo();
-			subtitle_text += ' \u2013 ';
-			subtitle_text += end.format_indo();
+			var end = erb_to_date(next_important_contest.data('end-time'));
+			var results = erb_to_date(next_important_contest
+				.data('result-time'));
+			var results_released = next_important_contest
+				.data('results-released');
+			title_text = next_important_contest.data('name');
 
-			if (start > current) {
-				button_text = current.indo_go_to(start);
+			if (results_released) {
+				button_text = 'Hasil sudah diumumkan!';
+			} else if (current > results) {
+				subtitle_text = ' Dikarenakan berbagai halangan, hasil kontes' +
+					' belum keluar. Mohon maaf atas ketidaknyamannya dan' +
+					' mohon bersabar :(';
+				$('#home-btn-daftar').hide();
+			} else if (current > end) {
+				// Show time to results
+				subtitle_text = 'Hasil diumumkan paling telat ' +
+						results.format_indo();
+				button_text = current.indo_go_to(results);
 			} else {
-				button_text = 'Kontes sudah dimulai!';
+				// Show time to next contest start
+				var start = erb_to_date(next_important_contest.data(
+							'start-time'));
+				subtitle_text = start.format_indo();
+				subtitle_text += ' \u2013 ';
+				subtitle_text += end.format_indo();
+
+				if (start > current) {
+					button_text = current.indo_go_to(start);
+				} else {
+					button_text = 'Kontes sudah dimulai!';
+				}
 			}
 		}
-
+		$('#next-contest-name').text(title_text);
 		$('#next-contest-time').text(subtitle_text);
 		$('#home-btn-daftar').text(button_text);
 	}
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
 	var next_contest = $('#welcome-contest-data');
 	if (next_contest.length !== 0) {
-		var start = erb_to_date(next_contest.data('start-time'));
-		var end = erb_to_date(next_contest.data('end-time'));
-		var name = next_contest.data('name');
 		var contest_text;
 
-		if (next_contest.data('currently-in-contest')) {
-			contest_text = 'KONTES SUDAH DIMULAI!';	
+		if (next_contest.data('name') === null) {
+			contest_text = '';
 		} else {
-			contest_text = start.format_indo() + '\u2013' + end.format_indo();
+			var start = erb_to_date(next_contest.data('start-time'));
+			var end = erb_to_date(next_contest.data('end-time'));
+			var name = next_contest.data('name');
+
+			if (next_contest.data('currently-in-contest')) {
+				contest_text = 'Kontes berikutnya: ' + name +
+					'(KONTES SUDAH DIMULAI!)';
+			} else {
+				contest_text = 'Kontes berikutnya: ' + name + ' (' + start.format_indo() + '\u2013' + end.format_indo() + ')';
+			}
 		}
-		$('#welcome-next-contest').text('Kontes berikutnya: '
-			+ name + ' (' + contest_text + ')');
+		$('#welcome-next-contest').text(contest_text);
 	}
 });
