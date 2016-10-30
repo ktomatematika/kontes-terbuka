3a2f8232c4b1bc9cdb53ef9337f9cfbaf4a07b83
woenardi <jonathanmulyawan@gmail.com>
2016-10-31 02:04:55 +0800

    new model after migration

--- app/models/market_order.rb
+++ app/models/market_order.rb
@@ -2,19 +2,22 @@
 #
 # Table name: market_orders
 #
-#  id             :integer          not null, primary key
-#  user_id        :integer
-#  market_item_id :integer
-#  quantity       :integer
-#  memo           :text
-#  status         :integer
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
+#  id                   :integer          not null, primary key
+#  point_transaction_id :integer
+#  market_item_id       :integer
+#  quantity             :integer
+#  email                :string
+#  phone                :string
+#  user_memo            :text
+#  internal_memo        :text
+#  status               :integer
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
 #
 # Indexes
 #
-#  index_market_orders_on_market_item_id  (market_item_id)
-#  index_market_orders_on_user_id         (user_id)
+#  index_market_orders_on_market_item_id        (market_item_id)
+#  index_market_orders_on_point_transaction_id  (point_transaction_id)
 #
 
 class MarketOrder < ActiveRecord::Base
