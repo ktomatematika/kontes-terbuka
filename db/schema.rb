3dbea3bc8d00504ecbbeed7a72f53ca032d9a0d0
donjar <7744han@gmail.com>
2016-12-17 07:23:56 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -116,6 +116,17 @@ ActiveRecord::Schema.define(version: 20161014091434) do
     t.integer  "price",       :null=>false
     t.datetime "created_at",  :null=>false
     t.datetime "updated_at",  :null=>false
+    t.integer  "quantity"
+  end
+
+  create_table "market_orders", force: :cascade do |t|
+    t.integer  "user_id",        :index=>{:name=>"index_market_orders_on_user_id", :using=>:btree}
+    t.integer  "market_item_id", :index=>{:name=>"index_market_orders_on_market_item_id", :using=>:btree}
+    t.integer  "quantity"
+    t.text     "memo"
+    t.integer  "status"
+    t.datetime "created_at",     :null=>false
+    t.datetime "updated_at",     :null=>false
   end
 
   create_table "notifications", force: :cascade do |t|
