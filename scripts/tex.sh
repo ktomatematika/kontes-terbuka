45c91e85c0d8138c5a2b6205acdb31eaa04615cb
donjar <7744han@gmail.com>
2016-12-23 12:28:41 +0800

    change functional test again

--- scripts/tex.sh
+++ scripts/tex.sh
@@ -2,7 +2,6 @@ wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xzf install-tl-unx.tar.gz
 cd install-tl-*
 sudo ./install-tl -profile=../scripts/texlive.profile
-export PATH="/usr/local/texlive/2016/bin/x86_64-linux:$PATH"
 printf "\nexport PATH=/usr/local/texlive/2016/bin/x86_64-linux:\$PATH" >> ~/.bashrc
 cd ..
 rm -r install-tl-*
