cf9a16a0c1eee357e613a66a940790a09151c16c
donjar <7744han@gmail.com>
2016-12-24 00:46:36 +0800

    use latest firefox

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -2,6 +2,7 @@ GECKODRIVER_VERSION=v0.11.1
 
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
+sleep 3
 
 wget https://github.com/mozilla/geckodriver/releases/download/${GECKODRIVER_VERSION}/geckodriver-${GECKODRIVER_VERSION}-linux64.tar.gz
 tar -xf geckodriver-${GECKODRIVER_VERSION}-linux64.tar.gz
