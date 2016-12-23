369dc4f9b754981a323142928203803ae4ac2c54
donjar <7744han@gmail.com>
2016-12-24 00:51:53 +0800

    use timeout 180

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -52,7 +52,15 @@ module ActionDispatch
   class IntegrationTest
     # Make the Capybara DSL available in all integration tests
     include Capybara::DSL
-    Capybara.default_driver = :selenium
+
+    Capybara.register_driver :long_selenium do |app|
+      client = Selenium::WebDriver::Remote::Http::Default.new
+      client.timeout = 180 # instead of the default 60
+      Capybara::Selenium::Driver.new(app, browser: :firefox,
+                                     http_client: client)
+    end
+
+    Capybara.default_driver = :long_selenium
 
     # Reset sessions and driver between tests
     # Use super wherever this method is redefined in your
