b74d9b8a56de79f49d696cbf59edd527e7af40a3
donjar <7744han@gmail.com>
2016-12-24 16:00:04 +0800

    enforce 80 char limits

--- test/lib/mailgun_test.rb
+++ test/lib/mailgun_test.rb
@@ -3,10 +3,12 @@ require 'test_helper'
 class MailgunTest < ActiveSupport::TestCase
   test 'send normal email' do
     assert_equal Mailgun.send_message(to: 'a@b.com', text: 'asdf'),
-      { to: 'a@b.com',
-        from: 'Kontes Terbuka Olimpiade Matematika <mail@ktom.tomi.or.id>',
-        text: "Salam sejahtera,\n\nasdf\n\nSalam,\nTim KTO Matematika" },
-        'Sending a normal email does not even work.'
+                 { to: 'a@b.com',
+                   from: 'Kontes Terbuka Olimpiade Matematika ' \
+                   '<mail@ktom.tomi.or.id>',
+                   text: "Salam sejahtera,\n\nasdf\n\n" \
+                   "Salam,\nTim KTO Matematika" },
+                 'Sending a normal email does not even work.'
   end
 
   test 'send email with to many separated by commas should fail' do
@@ -24,21 +26,25 @@ class MailgunTest < ActiveSupport::TestCase
   test 'send email with to many array with force_to_many should pass' do
     assert_equal Mailgun.send_message(to: 'a@b.com,c@d.com',
                                       text: 'asdf', force_to_many: true),
-      { to: 'a@b.com,c@d.com',
-        from: 'Kontes Terbuka Olimpiade Matematika <mail@ktom.tomi.or.id>',
-        text: "Salam sejahtera,\n\nasdf\n\nSalam,\nTim KTO Matematika" },
-        'Sending an email with force_to_many does not work.'
+                 { to: 'a@b.com,c@d.com',
+                   from: 'Kontes Terbuka Olimpiade Matematika ' \
+                   '<mail@ktom.tomi.or.id>',
+                   text: "Salam sejahtera,\n\nasdf\n\n" \
+                   "Salam,\nTim KTO Matematika" },
+                 'Sending an email with force_to_many does not work.'
   end
 
   test 'send email with contest params should have contest added to subject' do
     contest = create(:contest, name: 'Kontes Halo')
     assert_equal Mailgun.send_message(to: 'a@b.com', contest: contest,
                                       text: 'asdf', subject: 'Subjek'),
-      { to: 'a@b.com',
-        from: 'Kontes Terbuka Olimpiade Matematika <mail@ktom.tomi.or.id>',
-        text: "Salam sejahtera,\n\nasdf\n\nSalam,\nTim KTO Matematika",
-        subject: 'Kontes Halo: Subjek' },
-        'Sending an email with contest param does not work.'
+                 { to: 'a@b.com',
+                   from: 'Kontes Terbuka Olimpiade Matematika ' \
+                   '<mail@ktom.tomi.or.id>',
+                   text: "Salam sejahtera,\n\nasdf\n\n" \
+                   "Salam,\nTim KTO Matematika",
+                   subject: 'Kontes Halo: Subjek' },
+                 'Sending an email with contest param does not work.'
   end
 
   test 'send email with bcc_array should put the bcc_array to bcc' do
@@ -46,17 +52,21 @@ class MailgunTest < ActiveSupport::TestCase
                                       bcc: 'c@d.com',
                                       bcc_array: ['e@f.com', 'g@h.com'],
                                       text: 'asdf'),
-      { to: 'a@b.com', bcc: 'c@d.com,e@f.com,g@h.com',
-        from: 'Kontes Terbuka Olimpiade Matematika <mail@ktom.tomi.or.id>',
-        text: "Salam sejahtera,\n\nasdf\n\nSalam,\nTim KTO Matematika", },
-        'Sending an email with bcc_array does not work.'
+                 { to: 'a@b.com', bcc: 'c@d.com,e@f.com,g@h.com',
+                   from: 'Kontes Terbuka Olimpiade Matematika ' \
+                   '<mail@ktom.tomi.or.id>',
+                   text: "Salam sejahtera,\n\nasdf\n\n" \
+                   "Salam,\nTim KTO Matematika" },
+                 'Sending an email with bcc_array does not work.'
   end
 
   test 'send email without to adds the to' do
     assert_equal Mailgun.send_message(text: 'asdf'),
-      { to: 'mail@ktom.tomi.or.id',
-        from: 'Kontes Terbuka Olimpiade Matematika <mail@ktom.tomi.or.id>',
-        text: "Salam sejahtera,\n\nasdf\n\nSalam,\nTim KTO Matematika", },
-        'Sending an email without to adds the sender email to the to.'
+                 { to: 'mail@ktom.tomi.or.id',
+                   from: 'Kontes Terbuka Olimpiade Matematika ' \
+                   '<mail@ktom.tomi.or.id>',
+                   text: "Salam sejahtera,\n\nasdf\n\n" \
+                   "Salam,\nTim KTO Matematika" },
+                 'Sending an email without to adds the sender email to the to.'
   end
 end
--- test/tasks/facebook_post_test.rb
+++ test/tasks/facebook_post_test.rb
@@ -11,44 +11,52 @@ class FacebookPostTest < ActiveSupport::TestCase
   test 'contest_starting' do
     assert_equal @f.contest_starting('10 jam'),
                  "Salam sejahtera,\n\n#{@c} akan dimulai dalam waktu 10 jam. " \
-                   'Ayok segera daftar di website kami di https://ktom.tomi.or.id jika ' \
-                   'Anda belum!',
+                   'Ayok segera daftar di website kami di ' \
+                   'https://ktom.tomi.or.id jika Anda belum!',
                  'FacebookPost contest_starting text is not accurate.'
   end
 
   test 'contest_started' do
     assert_equal @f.contest_started,
-                 "Salam sejahtera,\n\n#{@c} sudah dimulai! Silakan membuka soalnya di " \
+                 "Salam sejahtera,\n\n#{@c} sudah dimulai! Silakan " \
+                 'membuka soalnya di ' \
                    "#{contest_url @c}. Selamat mengerjakan! :D",
                  'FacebookPost contest_started text is not accurate.'
   end
 
   test 'contest_ending' do
     assert_equal @f.contest_ending('10 jam'),
-                 "Salam sejahtera,\n\nHanya mengingatkan saja, #{@c} akan berakhir " \
+                 "Salam sejahtera,\n\nHanya mengingatkan saja, " \
+                 "#{@c} akan berakhir " \
                    "dalam waktu 10 jam.\nSiap-siap mengumpulkan segala " \
                    "pekerjaan Anda di #{contest_url @c}.\n\n" \
-                   'Antisipasi segala kegagalan teknis. Ingat, kami hampir tidak pernah ' \
-                   'memberikan waktu tambahan.',
+                   'Antisipasi segala kegagalan teknis. Ingat, kami ' \
+                   'hampir tidak pernah memberikan waktu tambahan.',
                  'FacebookPost contest_ending text is not accurate.'
   end
 
   test 'results_released' do
     assert_equal @f.results_released,
-                 "Salam sejahtera,\n\nHasil #{@c} sudah keluar! Silakan cek di:\n " \
+                 "Salam sejahtera,\n\nHasil #{@c} sudah keluar! " \
+                 "Silakan cek di:\n " \
                    "#{contest_url @c}.\n\nSelamat bagi yang mendapatkan " \
-                   'penghargaan! Jika Anda belum beruntung, jangan berkecil hati karena ' \
-                   "masih ada kontes-kontes berikutnya.\n\nMengenai sertifikat, Anda " \
-                   'perlu mengisi feedback kontes untuk mendapatkannya. Feedback ini bisa ' \
+                   'penghargaan! Jika Anda belum beruntung, jangan ' \
+                   'berkecil hati karena ' \
+                   "masih ada kontes-kontes berikutnya.\n\nMengenai " \
+                   'sertifikat, Anda ' \
+                   'perlu mengisi feedback kontes untuk mendapatkannya. ' \
+                   'Feedback ini bisa ' \
                    'diisi di halaman yang sama dengan hasil kontes, yaitu ' \
                    "#{contest_url @c}. Anda perlu mendapatkan setidaknya " \
-                   "#{UserContest::CUTOFF_CERTIFICATE} poin untuk mendapatkan sertifikat.",
+                   "#{UserContest::CUTOFF_CERTIFICATE} poin untuk " \
+                   'mendapatkan sertifikat.',
                  'FacebookPost results_released text is not accurate.'
   end
 
   test 'feedback_ending' do
     assert_equal @f.feedback_ending('10 jam'),
-                 "Salam sejahtera,\n\nHanya mengingatkan saja, waktu pengisian " \
+                 "Salam sejahtera,\n\nHanya mengingatkan " \
+                 'saja, waktu pengisian ' \
                    "feedback #{@c} ditutup 10 jam lagi. Anda bisa mengisi " \
                    "feedback di #{contest_url @c}. Ingat, salah satu syarat " \
                    'mendapatkan sertifikat adalah mengisi feedback ini.',
@@ -57,10 +65,13 @@ class FacebookPostTest < ActiveSupport::TestCase
 
   test 'certificate_sent' do
     assert_equal @f.certificate_sent,
-                 "Salam sejahtera,\n\nSertifikat untuk #{@c} sudah dikirim. Coba " \
-                   'cek email Anda! Jika Anda tidak mendapatkannya, berarti Anda tidak ' \
+                 "Salam sejahtera,\n\nSertifikat untuk #{@c} sudah " \
+                 'dikirim. Coba ' \
+                   'cek email Anda! Jika Anda tidak mendapatkannya, ' \
+                   'berarti Anda tidak ' \
                    'memenuhi syarat mendapatkan sertifikat, yakni ' \
-                   "minimal #{UserContest::CUTOFF_CERTIFICATE} poin di kontes dan " \
+                   "minimal #{UserContest::CUTOFF_CERTIFICATE} poin di ' \
+                   'kontes dan " \
                    'mengisi semua feedback.',
                  'FacebookPost certificate_sent text is not accurate.'
   end
