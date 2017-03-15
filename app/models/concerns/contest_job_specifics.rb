5172c9f76792c9b47146f53a56ad86bd391e0625
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-03-15 11:24:37 +0800

    log delayed job

--- app/models/concerns/contest_job_specifics.rb
+++ app/models/concerns/contest_job_specifics.rb
@@ -7,8 +7,8 @@ module ContestJobSpecifics
     user_contests.processed.each do |uc|
       PointTransaction.create(point: uc.contest_points, user_id: uc.user_id,
                               description: uc.contest.to_s)
+      dj_log uc: uc.id
     end
-    Ajat.info "award_points|id:#{id}"
   end
 
   def purge_panitia
@@ -17,7 +17,7 @@ module ContestJobSpecifics
     user_contests.where(user_id: User.joins(:roles)
                                      .where('roles.name' => panitia_roles))
                  .destroy_all
-    Ajat.info "purge_panitia|id:#{id}"
+    dj_log
   end
 
   def send_most_answers
@@ -29,6 +29,7 @@ module ContestJobSpecifics
                          text: 'Berikut ini jawaban terbanyak di kontes ' \
                          'ini, mohon dibandingkan dan dicek ulang ' \
                          "bila jawaban aslinya beda.\n#{answers}"
+    dj_log
   end
 
   def check_submissions
@@ -38,34 +39,47 @@ module ContestJobSpecifics
       Mailgun.send_message to: ls.user_contest.user.email,
                            contest: self, subject: data.subject.get(binding),
                            text: data.text.get(binding)
+      dj_log ls: ls.id
     end
   end
 
   def check_veteran
     users.each do |u|
-      gold = 0
-      u.user_contests.include_marks.each do |uc|
-        gold += 1 if uc.total_mark >= uc.contest.gold_cutoff
-      end
+      next if u.has_role? :veteran
 
-      next unless gold >= 3 && !u.has_role?(:veteran)
+      gold = u.user_contests.include_marks.count do |uc|
+        uc.total_mark >= uc.contest.gold_cutoff
+      end
+      next unless gold >= 3
+
       u.add_role :veteran
       data = Social.contest_job_specifics.check_veteran
       Mailgun.send_message to: u.email, subject: data.subject.get(binding),
                            text: data.text.get(binding)
+
+      dj_log uc: uc.id
     end
-    Ajat.info "check_veteran|id:#{id}"
   end
 
   def send_certificates
     full_feedback_user_contests.eligible_score.each do |uc|
       CertificateManager.new(uc).run
+
+      dj_log uc: uc.id
     end
-    Ajat.info "send_certificates|id:#{id}"
   end
 
   def do_if_not_time(run_at, object, method, *args)
     return if Time.zone.now >= run_at
     object.delay(run_at: run_at, queue: "contest_#{id}").__send__(method, *args)
+  end
+
+  def dj_log(**kwargs)
+    dj_logger = Logger.new(Rails.root.join('log', 'delayed_job.log'))
+
+    original_method = caller_locations(1, 1)[0].label
+    string_kwargs = kwargs.map { |pair| "#{pair[0]}=#{pair[1]}" }.join('|')
+
+    dj_logger.debug "#{original_method}|c=#{id}|#{string_kwargs}"
   end
 end
