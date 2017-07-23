5a5c8c7a7de79339b1fddcb5712c86c73ee21ad4
Herbert Ilhan <htanujaya@twitter.com>
2017-07-23 23:38:53 +0800

    dropped squeel for most of user contest queries

--- app/models/concerns/user_contest_scope.rb
+++ app/models/concerns/user_contest_scope.rb
@@ -3,58 +3,32 @@
 module UserContestScope
   extend ActiveSupport::Concern
   included do
-    # Show short marks on model objects. Short marks only
-    # Usage: UserContest.short_marks
-    scope(:short_marks, lambda {
-      joins('LEFT OUTER JOIN short_submissions ON \
-    short_submissions.user_contest_id = user_contests.id')
-      .joins('LEFT OUTER JOIN short_problems ON \
-    short_submissions.short_problem_id = short_problems.id')
-      .group(:id)
-      .select('user_contests.id, SUM(CASE WHEN \
-    short_submissions.answer = short_problems.answer \
-    THEN 1 ELSE 0 END) AS short_mark')
-    })
-
-    # Show long marks on model objects. Long marks only
-    scope(:long_marks, lambda {
-      joins { long_submissions.outer }
-        .group(:id)
-        .select('user_contests.id as id, ' \
-        'sum(coalesce(long_submissions.score, 0)) as long_mark')
-    })
-
-    # Show both short marks and long marks. Short and long marks
+    # Show both short marks and long marks.
     scope(:include_marks, lambda {
-      sm = joins do
-        UserContest.short_marks.as(short_marks).on { id == short_marks.id }
-      end
-      short_and_long_marks = sm.joins do
-        UserContest.long_marks.as(long_marks).on { id == long_marks.id }
-      end
-      short_and_long_marks.select do
-        ['user_contests.*', 'short_marks.short_mark',
-         'long_marks.long_mark', '(short_marks.short_mark + ' \
-                   'long_marks.long_mark) as total_mark']
-      end
+      select('*, short_mark + long_mark AS total_mark').from(
+        UserContest.joins('LEFT OUTER JOIN short_submissions ON ' \
+              'short_submissions.user_contest_id = user_contests.id')
+        .joins('LEFT OUTER JOIN short_problems ON ' \
+               'short_submissions.short_problem_id = short_problems.id')
+        .joins('LEFT OUTER JOIN long_submissions ON ' \
+              'long_submissions.user_contest_id = user_contests.id')
+        .group(:id)
+        .select('user_contests.*, ' \
+                'SUM(CASE WHEN short_submissions.answer = ' \
+                'short_problems.answer THEN 1 ELSE 0 END) AS short_mark, ' \
+                'SUM(COALESCE(long_submissions.score, 0)) AS long_mark'),
+        'user_contests'
+      )
     })
 
     # Show marks + award (emas/perak/perunggu)
     scope(:processed, lambda {
-      included = joins do
-        UserContest.include_marks.as(marks).on { id == marks.id }
-      end
-      included.joins { contest }
-              .select do
-        ['user_contests.*',
-         'marks.short_mark',
-         'marks.long_mark',
-         'marks.total_mark',
-         "case when marks.total_mark >= gold_cutoff then 'Emas'
-               when marks.total_mark >= silver_cutoff then 'Perak'
-               when marks.total_mark >= bronze_cutoff then 'Perunggu'
-               else '' end as award"]
-      end
+      select('*, ' \
+             "CASE WHEN total_mark >= gold_cutoff THEN 'Emas' " \
+             "WHEN total_mark >= silver_cutoff THEN 'Perak' " \
+             "WHEN total_mark >= bronze_cutoff THEN 'Perunggu' " \
+             "ELSE '' END AS award")
+      .from(UserContest.include_marks.joins(:contest))
     })
 
     # Given a long problem ID, this shows table of user contest id
