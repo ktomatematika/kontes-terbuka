a854d70fc28a5c246103cf948c77d052f6228fa4
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-01-05 15:37:18 +0800

    About Us done

--- app/assets/javascripts/about_us_entries.js
+++ app/assets/javascripts/about_us_entries.js
@@ -1,4 +1,4 @@
-/* exported about_us_data */
+/* exported about_us_data alumni_data */
 var about_us_data = [
 	{
 		name: 'Herbert Ilhan Tanujaya',
@@ -11,16 +11,6 @@ var about_us_data = [
 		image: 'ricky.jpg',
 	},
 	{
-		name: 'Fransisca Andriani',
-		description: 'Fransisca Andriani (Cis) pernah beruntung mendapatkan medali perak OSN 2013 dan medali emas OSN 2015 meskipun kemampuannya tidak seberapa. Dia juga beruntung lagi di tahun 2016 saat diterima di Nanyang Technological University (NTU) untuk melanjutkan studinya dalam bidang Matematika. Peran Cis di KTO Matematika antara lain adalah sebagai pengisi kuota perempuan (karena tidak ada perempuan lain yang mau), desainer, pengelola utama OA LINE KTO Matematika, dan terkadang juga korektor jawaban kontes. Dia sangat suka menyanyi dan membaca. Dia juga pandai menggerakkan anggota wajahnya seperti hidung, telinga, dan alis.',
-		image: 'cis.jpg',
-	},
-	{
-		name: 'Rudi Adha Prihandoko',
-		description: 'Prihandoko Rudi barangkali adalah sosok yang paling terkenal di kalangan pelatihan nasional olimpiade matematika. Selain meraih medali emas OSN SMA 2004, Rudi mendapatkan medali perunggu Asian Pacific Mathematics Olympiad (APMO) 2007 dan meraih honorable mention di IMO 2007. Dia juga menjadi asisten pelatihan dari tahun 2007 dan menjadi pembina dari tahun 2013 sampai sekarang; dia kenal hampir semua peserta IMO dari tahun 2001 sampai sekarang! Sebagai salah satu perintis KTO Matematika, Rudi terus membantu di berbagai hal.',
-		image: 'rudi.jpg',
-	},
-	{
 		name: 'Made Tantrawan',
 		description: 'Made Tantrawan merupakan salah satu peraih medali emas bidang matematika pada OSN 2008. Pada saat menempuh pendidikan sarjananya, pria yang akrab disapa Wawan ini sempat mewakili Indonesia di ajang International Mathematics Competition (IMC) di Bulgaria sebanyak 3 kali. Adapun dalam kompetisi tersebut, ia berhasil meraih 2nd prize di tahun 2010, 3rd prize di tahun 2011, dan 2nd prize kembali di tahun 2012. Saat ini, ia sedang menempuh studi S3 di National University Singapore (NUS) dalam bidang matematika. Di sela-sela studinya, ia turut membantu KTO Matematika dalam beberapa bidang seperti tim soal, tim koreksi dan tim pengolah.',
 		image: 'wawan.jpg',
@@ -31,14 +21,32 @@ var about_us_data = [
 		image: 'yoyo.jpg',
 	},
 	{
-		name: 'Erwin Eko Wahyudi',
-		description: 'Pria yang sering disapa Erwin ini merupakan salah satu perintis pertama KTO Matematika. Berangkat dari medali perunggu di OSN 2011, dia terus menjalankan passionnya terhadap matematika sehingga ia berhasil mendapatkan medali emas di ONMIPA 2015. Orang yang saat ini sedang kuliah Teknik Komputer di UGM ini terus menerapkan ilmunya dengan mengurus bagian teknis KTO Matematika.',
-		image: 'erwin.jpg',
-	},
-	{
 		name: 'Kevin Christian Wibisono',
 		description: 'Kevin Christian Wibisono ialah seorang pencinta matematika yang diberi kesempatan untuk mendemonstrasikan kegemarannya sekaligus merepresentasikan negaranya dalam ajang olimpiade matematika paling prestisius bagi siswa sekolah dasar dan menengah. Saat ini, Kevin sedang menempuh pendidikan tinggi di National University of Singapore (NUS). Keinginannya untuk mendalami matematika membuatnya memilih jurusan Applied Mathematics. Sebagai bentuk kontribusi kepada pendidikan olimpiade matematika di Indonesia, Kevin mendedikasikan dirinya menjadi Ketua Tim Pengolah KTO Matematika, yang tanggung jawabnya meliputi pemeriksaan kualitas soal, jawaban, dan pembuatan buku. Pada waktu senggang, Kevin suka menonton film kriminal dan mengerjakan soal-soal olimpiade matematika.',
 		image: 'kevin.jpg',
+	},
+	{
+		name: 'Muhammad Afifurrahman',
+		description: 'Muhammad Afifurrahman, yang biasa dipanggil Afif, adalah seorang lajang muda yang sedang menuntut ilmu di FMIPA ITB. Lelaki asli Banjarmasin ini menjalani masa sekolah menengahnya selama enam tahun di Banten, dan tercatat pernah mewakili provinsi Banten dalam OSN 2014 dan OSN 2015 di bidang matematika, berturut-turut mendapatkan medali perak dan emas. Pria penggemar mie dan FM ini juga tercatat berhasil menembus tahap tiga pelatihan nasional calon peserta IMO di 2016, dan berhasil membuat jumlah pacarnya menjadi sepuluh kali lipat lebih banyak selama menjalani pelatihan nasional. Saat ini, pria yang mengaku ganteng ini sedang membantu pengurusan akun-akun media sosial KTO Matematika, menyumbang soal untuk kontes bulanan, dan berkeliling di internet untuk mencari hal-hal yang menarik.',
+		image: 'afif.jpg',
+	},
+];
+
+var alumni_data = [
+	{
+		name: 'Fransisca Andriani',
+		description: 'Fransisca Andriani (Cis) pernah beruntung mendapatkan medali perak OSN 2013 dan medali emas OSN 2015 meskipun kemampuannya tidak seberapa. Dia juga beruntung lagi di tahun 2016 saat diterima di Nanyang Technological University (NTU) untuk melanjutkan studinya dalam bidang Matematika. Peran Cis di KTO Matematika antara lain adalah sebagai pengisi kuota perempuan (karena tidak ada perempuan lain yang mau), desainer, pengelola utama OA LINE KTO Matematika, dan terkadang juga korektor jawaban kontes. Dia sangat suka menyanyi dan membaca. Dia juga pandai menggerakkan anggota wajahnya seperti hidung, telinga, dan alis.',
+		image: 'cis.jpg',
+	},
+	{
+		name: 'Rudi Adha Prihandoko',
+		description: 'Prihandoko Rudi barangkali adalah sosok yang paling terkenal di kalangan pelatihan nasional olimpiade matematika. Selain meraih medali emas OSN SMA 2004, Rudi mendapatkan medali perunggu Asian Pacific Mathematics Olympiad (APMO) 2007 dan meraih honorable mention di IMO 2007. Dia juga menjadi asisten pelatihan dari tahun 2007 dan menjadi pembina dari tahun 2013 sampai sekarang; dia kenal hampir semua peserta IMO dari tahun 2001 sampai sekarang! Sebagai salah satu perintis KTO Matematika, Rudi terus membantu di berbagai hal.',
+		image: 'rudi.jpg',
+	},
+	{
+		name: 'Erwin Eko Wahyudi',
+		description: 'Pria yang sering disapa Erwin ini merupakan salah satu perintis pertama KTO Matematika. Berangkat dari medali perunggu di OSN 2011, dia terus menjalankan passionnya terhadap matematika sehingga ia berhasil mendapatkan medali emas di ONMIPA 2015. Orang yang saat ini sedang kuliah Teknik Komputer di UGM ini terus menerapkan ilmunya dengan mengurus bagian teknis KTO Matematika.',
+		image: 'erwin.jpg',
 	},
 	{
 		name: 'Farras Mohammad Hibban Faddila',
@@ -55,23 +63,21 @@ var about_us_data = [
 		description: 'Bivan Alzacky Harmanto adalah salah satu siswa yang berhasil mengikuti IMO 2012 dan IMO 2013 dan menyumbangkan medali perunggu untuk Indonesia. Saat ini Bivan sedang menempuh studi S1 jurusan Ilmu Komputer di Korea Advanced Institute of Science and Technology (KAIST), yang merupakan universitas nomor 1 di Korea untuk bidang teknik. Saat ini, Bivan merupakan salah satu anggota tim koreksi KTO Matematika. Selain gemar  mengotak-atik soal-soal Matematika (khususnya geometri) dan komputer, Bivan juga gemar mengecek Facebook tanpa alasan yang jelas (sekadar buka dan lihat-lihat saja), membagi ceritanya di blognya, dan bermain game di laptopnya. Oh iya satu hal lagi, board game sekarang menjadi salah satu kegemarannya, berkat salah satu temannya di KAIST yang terbilang sangat gila terhadap board game.',
 		image: 'bivan.jpg',
 	},
-	{
-		name: 'Muhammad Afifurrahman',
-		description: 'Muhammad Afifurrahman, yang biasa dipanggil Afif, adalah seorang lajang muda yang sedang menuntut ilmu di FMIPA ITB. Lelaki asli Banjarmasin ini menjalani masa sekolah menengahnya selama enam tahun di Banten, dan tercatat pernah mewakili provinsi Banten dalam OSN 2014 dan OSN 2015 di bidang matematika, berturut-turut mendapatkan medali perak dan emas. Pria penggemar mie dan FM ini juga tercatat berhasil menembus tahap tiga pelatihan nasional calon peserta IMO di 2016, dan berhasil membuat jumlah pacarnya menjadi sepuluh kali lipat lebih banyak selama menjalani pelatihan nasional. Saat ini, pria yang mengaku ganteng ini sedang membantu pengurusan akun-akun media sosial KTO Matematika, menyumbang soal untuk kontes bulanan, dan berkeliling di internet untuk mencari hal-hal yang menarik.',
-		image: 'afif.jpg',
-	},
 ];
 
 // Randomize the order!
-var current_index = about_us_data.length;
-var temporary_value;
-var random_index;
+function randomize_array(arr) {
+	var current_index = arr.length;
 
-while (current_index !== 0) {
-	random_index = Math.floor(Math.random() * current_index);
-	current_index--;
+	while (current_index !== 0) {
+		var random_index = Math.floor(Math.random() * current_index);
+		current_index--;
 
-	temporary_value = about_us_data[current_index];
-	about_us_data[current_index] = about_us_data[random_index];
-	about_us_data[random_index] = temporary_value;
+		var temporary_value = arr[current_index];
+		arr[current_index] = arr[random_index];
+		arr[random_index] = temporary_value;
+	}
 }
+
+randomize_array(about_us_data);
+randomize_array(alumni_data);
