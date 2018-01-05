817ee2ceec20e604702666ab0aaaeaaeb403d22a
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-01-05 15:37:18 +0800

    About Us done

--- app/assets/javascripts/about_us.js
+++ app/assets/javascripts/about_us.js
@@ -6,36 +6,41 @@ $(document).ready(function() {
 	var MD_ROW = BOOTSTRAP_ROW / MD_COLUMNS;
 	var SM_ROW = BOOTSTRAP_ROW / SM_COLUMNS;
 
-	var len_round_md = MD_COLUMNS *
-		Math.ceil(about_us_data.length / MD_COLUMNS);
+	function add_data(array, tag) {
+		var len_round_md = MD_COLUMNS *
+			Math.ceil(array.length / MD_COLUMNS);
 
-	for (var i = 0; i < len_round_md; i++) {
-		var about_us_obj = about_us_data[i];
+		for (var i = 0; i < len_round_md; i++) {
+			var about_us_obj = array[i];
 
-		// Add about us objects if not undefined
-		if (typeof about_us_obj !== 'undefined') {
-			$('#daftar-panitia').append(
+			// Add about us objects if not undefined
+			if (typeof about_us_obj !== 'undefined') {
+				tag.append(
 					'<div class="col-md-' + MD_ROW + ' col-sm-' + SM_ROW +
 					' about-us-person "' +
 					'data-name="' + about_us_obj.name + '" ' +
 					'data-description="' + about_us_obj.description + '">' +
-					'<h3>' + about_us_obj.name + '</h3>' +
 					$('#about-us-pics').data(about_us_obj.image) +
+					'<h3>' + about_us_obj.name + '</h3>' +
 					'</div>');
-		}
+			}
 
-		// Clearfix + add placeholders for about us description
-		var clearfix_classes = 'clearfix visible-xs';
-		if (i % SM_COLUMNS === SM_COLUMNS - 1) {
-			clearfix_classes += ' visible-sm';
-		}
-		if (i % MD_COLUMNS === MD_COLUMNS - 1) {
-			clearfix_classes += ' visible-md visible-lg';
-		}
-		$('#daftar-panitia').append(
+			// Clearfix + add placeholders for about us description
+			var clearfix_classes = 'clearfix visible-xs';
+			if (i % SM_COLUMNS === SM_COLUMNS - 1) {
+				clearfix_classes += ' visible-sm';
+			}
+			if (i % MD_COLUMNS === MD_COLUMNS - 1) {
+				clearfix_classes += ' visible-md visible-lg';
+			}
+			tag.append(
 				'<div class="' + clearfix_classes + '"></div>' +
 				'<div class="about-us-description"></div>');
+		}
 	}
+
+	add_data(about_us_data, $('#daftar-panitia'));
+	add_data(alumni_data, $('#alumni-panitia'));
 
 	function reset_descriptions() {
 		$('.about-us-description').slideUp('fast');
