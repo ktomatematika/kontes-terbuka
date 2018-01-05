ebdbb22655412812c7022d149510661989343b65
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-01-05 15:00:17 +0800

    Rubocop

--- test/support.rb
+++ test/support.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/AutoResourceCleanup
 PDF = File.open(Rails.root.join('test', 'support', 'a.pdf'), 'r')
 TEX = File.open(Rails.root.join('test', 'support', 'a.tex'), 'r')
