44bf2cce7afe67c76d8bc5455b9fe1ba5a262ea6
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-01-25 22:05:43 +0800

    Guard

--- config/initializers/time_parser.rb
+++ config/initializers/time_parser.rb
@@ -4,9 +4,8 @@ module TimeParser
   refine String do
     def parse_hhmmss
       matches = /(\d\d):(\d\d):(\d\d)/.match(self)
-      if matches
-        matches[1].to_i.hours + matches[2].to_i.minutes + matches[3].to_i.seconds
-      end
+      return unless matches
+      matches[1].to_i.hours + matches[2].to_i.minutes + matches[3].to_i.seconds
     end
   end
 end
