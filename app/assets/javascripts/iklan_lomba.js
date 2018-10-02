b3e71717680d4615597c7651d80d02b0d0768e21
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-10-02 19:00:42 +0100

    + LMNAS UGM 29

--- app/assets/javascripts/iklan_lomba.js
+++ app/assets/javascripts/iklan_lomba.js
@@ -1,6 +1,6 @@
-var iklan_title = '';
-var iklan_desc = 'Tunggu kerjasama kami dengan kontes berikutnya!';
-var iklan_link = '';
+var iklan_title = 'LM UGM 29';
+var iklan_desc = 'Pendaftaran: 24 Sep - 14 Okt<br>Penyisihan: Minggu, 28 Oktober 2018<br>Final: 11 November 2018 di FMIPA UGM Yogyakarta';
+var iklan_link = 'http://lmnas.fmipa.ugm.ac.id';
 
 $(document).on('turbolinks:load', function() {
   if ($('#iklan-lomba').length !== 0) {
