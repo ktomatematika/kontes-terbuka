8b8c95c3e71dae33b672a5defa2f897adc57bdf6
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-10-10 20:58:44 +0100

    Separate FB Turbolinks

---
+++ app/assets/javascripts/facebook_turbolinks.js
@@ -0,0 +1,44 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '267896456882782',
+    xfbml      : true,
+    version    : 'v2.8'
+  });
+};
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Compatibility with Turbolinks 5
+(function($) {
+  var fbRoot;
+
+  function saveFacebookRoot() {
+    if ($('#fb-root').length) {
+      fbRoot = $('#fb-root').detach();
+    }
+  };
+
+  function restoreFacebookRoot() {
+    if (fbRoot != null) {
+      if ($('#fb-root').length) {
+        $('#fb-root').replaceWith(fbRoot);
+      } else {
+        $('body').append(fbRoot);
+      }
+    }
+
+    // Instance of Facebook SDK
+    if (typeof FB !== "undefined" && FB !== null) {
+      FB.XFBML.parse();
+    }
+  };
+
+  document.addEventListener('turbolinks:request-start', saveFacebookRoot)
+  document.addEventListener('turbolinks:load', restoreFacebookRoot)
+}(jQuery));
