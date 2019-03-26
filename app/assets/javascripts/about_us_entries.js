08461d5e97bbfedc990478ae01ef681050a65051
Herbert Ilhan Tanujaya <mail@herbert.id>
2019-03-26 11:09:03 +0800

    WIP about us

--- app/assets/javascripts/about_us_entries.js
+++ app/assets/javascripts/about_us_entries.js
@@ -21,19 +21,9 @@ var about_us_data = [
     image: 'yoyo.jpg',
   },
   {
-    name: 'Kevin Christian Wibisono',
-    description: 'Kevin Christian Wibisono ialah seorang pencinta matematika yang diberi kesempatan untuk mendemonstrasikan kegemarannya sekaligus merepresentasikan negaranya dalam ajang olimpiade matematika paling prestisius bagi siswa sekolah dasar dan menengah. Saat ini, Kevin sedang menempuh pendidikan tinggi di National University of Singapore (NUS). Keinginannya untuk mendalami matematika membuatnya memilih jurusan Applied Mathematics. Sebagai bentuk kontribusi kepada pendidikan olimpiade matematika di Indonesia, Kevin mendedikasikan dirinya menjadi Ketua Tim Pengolah KTO Matematika, yang tanggung jawabnya meliputi pemeriksaan kualitas soal, jawaban, dan pembuatan buku. Pada waktu senggang, Kevin suka menonton film kriminal dan mengerjakan soal-soal olimpiade matematika.',
-    image: 'kevin.jpg',
-  },
-  {
     name: 'Muhammad Afifurrahman',
     description: 'Muhammad Afifurrahman, bisa dipanggil Afif, tengah menempuh studi di Institut Teknologi Bandung sebagai mahasiswa sarjana Matematika. Lajang asli Banjarmasin ini pernah dua kali mengikuti OSN SMA (bidang matematika) pada 2014 dan 2015, berturut-turut menyabet medali perak dan emas. Penggemar mie instan dan sate ayam ini juga menembus pelatihan calon peserta IMO 2016 tahap 3 (11 besar). (pengaku) Penggemar Chrisye dan anime slice-of-life ini sangat senang menulis; baik puisi, cerpen, ataupun jawaban ujian. Saat ini, pria yang berharap bisa berjalan kaki jarak jauh ini membantu mengurus akun media sosial KTO Matematika, dan terkadang membantu serabutan untuk kegiatan-kegiatan rutin KTOM.',
     image: 'afif.png',
-  },
-  {
-    name: 'Kevin Winata',
-    description: 'Kevin Winata berasal dari Sibolga, sebuah kota di Sumatera Utara yang berpenduduk sekitar 85 ribu orang dan berjarak 350 km dari ibukota provinsinya, Medan. Penghobi kegiatan menggambar ini menyabet medali perak di OSN Matematika 2016, dan sekarang berkuliah di NTU (Nanyang Technological University) di Singapura, jurusan Computer Science. Pemain gitar, competitive programming, dan tenis meja ini merupakan salah satu pemegang amanah dalam pengecekan kualitas soal dan buku KTO Matematika agar sesuai dengan standar, dan turut mendesain beberapa poster KTOM.',
-    image: 'winata.png',
   },
   {
     name: 'William Kho',
@@ -51,14 +41,54 @@ var about_us_data = [
     image: 'rio.png',
   },
   {
-    name: 'Eunice Sukarto',
-    description: 'Eunice merupakan anggota perempuan KTO Matematika kedua setelah Fransisca Andriani (Cis). Kecintaannya terhadap matematika membuahkannya hasil manis berupa medali perunggu di OSN 2015 dan medali perak di OSN 2016. Saat ini, ia sedang melanjutkan studi S1 di UC Berkeley, California dalam bidang ilmu komputer. Peran Eunice di KTO Matematika adalah sebagai desainer, pengisi kuota perempuan (selain Cis) dan membantu mengecek kualitas soal setiap bulannya. Melukis, bermain piano klasik serta jalan-jalan merupakan pelariannya ketika penat belajar matematika. Melihat hasil lukisannya, dapat dikatakan Eunice merupakan pelukis beraliran naturalis, namun ia lebih suka disebut pelukis beraliran manusiawi.',
-    image: 'eunice.png',
-  },
-  {
     name: 'Rezky Arizaputra',
     description: 'Rezky Arizaputra atau kerap disapa Ekky adalah perwakilan Indonesia yang berhasil meraih dua medali perak di IMO pada tahun 2015 dan 2016. Di tengah kesibukan sebagai mahasiswa Teknik Komputer di NUS, Singapura Ekky masih menyempatkan waktunya membuat soal KTOM setiap bulannya dan membantu mengoreksi pekerjaan peserta. Ekky memiliki kemampuan matematika yang luar biasa sehingga kerap kali peserta pelatnas menyebut kemampuan matematika Ekky sebagai YME (Yang Maha Ekky). Selain gemar matematika, Ekky senang bermain piano dan travelling di waktu senggangnya. Dia juga senang bermain game Dota 2 dan sudah meraih medal divine 6!',
     image: 'ekky.png',
+  },
+  {
+    name: 'Abdillah Ahmad',
+    description: 'Abdillah Ahmad telah mencoba mengikuti OSN sejak tahun 2010, saat dia kelas 3 SD (walaupun waktu itu hanya sampai tingkat provinsi). Setelah 5 kali mencoba, akhirnya dia berhasil meraih medali perak dalam OSN 2015. Kini dia tengah mengenyam pendidikan di bangku SMA. Saat ini, dia menjadi bagian dalam tim koreksi KTOM. Selain mengutak-atik matematika, dia juga suka ngoding komputer. Di antara program yang telah dia buat adalah game "snake".',
+    image: 'abdillah.jpg',
+  },
+  {
+    name: 'Clifton Felix',
+    description: 'Clifton Felix adalah peserta OSN yang memperoleh medali perak pada OSN 2018. Menjadi seorang pelajar di Palembang, Sumatera Selatan tidak menghalanginya untuk berkontribusi dalam dunia olimpiade matematika. Sekarang ia telah menjadi tim soal dan tim koreksi KTOM.  Sebagai pelarian dari matematika, ia gemar memainkan games iklan Instagram (termasuk HAGO).',
+    image: 'clifton.png',
+  },
+  {
+    name: 'Russell Saerang',
+    description: 'Kecintaan Russell pada matematika sejak SMP membuatnya berhasil meraih medali perunggu di OSN 2018 SMA untuk provinsi Jakarta. Saat ini Russell merupakan salah satu pembuat soal dan juga korektor di KTO Matematika. Masih berada di Jakarta, Russell juga sedang duduk di bangku kelas 11. Tidak hanya gemar matematika, bila bosan ia juga suka mengerjakan soal kimia dan program coding. Kalau mencari orang yang garing, mungkin ia orang yang tepat!',
+    image: 'russell.jpg',
+  },
+  {
+    name: 'Rizky Maulana Hakim',
+    description: 'Rizky Maulana Hakim adalah salah satu alumni dan medalis OSN. Pertama kali mengikuti ajang OSN pada tahun 2018, Rizky langsung bisa meraih medali perak ke-6 pada bidang Matematika tingkat SMA, sebuah pencapaian luar biasa baginya. Sayang, OSN 2018 adalah kesempatan terakhir Rizky di kancah tersebut.  Rizky mulai masuk ke tim KTOM pada Agustus 2018, sebagai pengaju proposal soal untuk setiap kontes Bagi Rizky, ini adalah salah satu langkah kecilnya dalam usaha memajukan bangsa.  Mengenai hobi, bermain game, dan membalap adalah dua hobi terbesar bagi Rizky. Namun, semua hobi yang dimiliki Rizky tidak bisa mengalahkan kecintaannya terhadap Matematika.',
+    image: 'rizky.jpg',
+  },
+  {
+    name: 'Aaron Alvarado Kristanto Julistiono',
+    description: 'Aaron Alvarado masih harus berjuang untuk mencapai cita-cita. Sekarang dia sudah lolos pelatnas tahap 1 IMO dan akan menuju pelatnas tahap 2 di tahun depan. Aaron mulai menggeluti bidang matematika sejak SD dan sejak saat itu juga menjadi pejuang OSN. Meskipun sudah meraih medali emas di OSN SD 2014, OSN SMP 2016 dan OSN SMA 2018. Aaron masih harus banyak belajar dan menempa diri. Salah satu caranya dengan bergabung menjadi korektor KTOM.  Saat ini Aaron duduk di bangku kelas 10 di SMA Kristen Petra 1 Surabaya. Dengan berbagai kesibukannya di sekolah karena dia mengambil kelas cambridge, dia masih sempat bermain games dan juga nonton bioskop',
+    image: 'aaron.jpg',
+  },
+  {
+    name: 'Gabriela Erin Mariangel',
+    description: 'Aaron Alvarado masih harus berjuang untuk mencapai cita-cita. Sekarang dia sudah lolos pelatnas tahap 1 IMO dan akan menuju pelatnas tahap 2 di tahun depan. Aaron mulai menggeluti bidang matematika sejak SD dan sejak saat itu juga menjadi pejuang OSN. Meskipun sudah meraih medali emas di OSN SD 2014, OSN SMP 2016 dan OSN SMA 2018. Aaron masih harus banyak belajar dan menempa diri. Salah satu caranya dengan bergabung menjadi korektor KTOM.  Saat ini Aaron duduk di bangku kelas 10 di SMA Kristen Petra 1 Surabaya. Dengan berbagai kesibukannya di sekolah karena dia mengambil kelas cambridge, dia masih sempat bermain games dan juga nonton bioskop',
+    image: 'erin.jpg',
+  },
+  {
+    name: 'Audrey Felicio Anwar',
+    description: 'Halo! Nama saya Audrey Felicio Anwar dari SMAK 6 PENABUR Jakarta. Saya sekarang duduk di kelas 12. Saya adalah pemenang medali perak OSN 2018 bidang matematika di Padang, Sumatera Barat. Sebagai kelanjutan dari OSN, saya diberi kepercayaan untuk ikut pelatnas IMO 2019 tahap 1 dan puji Tuhan lolos ke tahap berikutnya. Dalam KTOM, saya bertugas sebagai pembuat soal sekaligus korektor. Hobi saya bermain games, musik, dan basket. Fun fact: bisa main gitar dan lumayan bisa nyanyi. Sekian perkenalan diri saya.'
+    image: 'audrey.jpg',
+  },
+  {
+    name: 'Ubay Ariq',
+    description: '',
+    image: 'ubay.jpg',
+  },
+  {
+    name: 'Vishandi Di',
+    description: 'Hi! Namaku Vishandi Rudy Keneta dan sekarang berdomisili di Pekanbaru, Riau. Sejak SD, aku sudah ikut berbagai lomba matematika. Sekarang aku masih kelas 12 di SMA Darma Yudha Pekanbaru. Sejak aku ikut lomba matematika, aku sudah pernah beberapa kali ikut OSN. Khusus untuk tingkat SMA, aku sudah pernah dapat perak di OSN 2017 dan perunggu di OSN 2018. Di sela-sela aktivitasku di sekolah, aku juga menjadi korektor di KTOM. Hobiku bermain basket, walaupun sebenarnya nggak jago amat sih.',
+    image: 'vishandi.png',
   },
 ];
 
@@ -92,6 +122,21 @@ var alumni_data = [
     name: 'Bivan Alzacky Harmanto',
     description: 'Bivan Alzacky Harmanto adalah salah satu siswa yang berhasil mengikuti IMO 2012 dan IMO 2013 dan menyumbangkan medali perunggu untuk Indonesia. Saat ini Bivan sedang menempuh studi S1 jurusan Ilmu Komputer di Korea Advanced Institute of Science and Technology (KAIST), yang merupakan universitas nomor 1 di Korea untuk bidang teknik. Saat ini, Bivan merupakan salah satu anggota tim koreksi KTO Matematika. Selain gemar  mengotak-atik soal-soal Matematika (khususnya geometri) dan komputer, Bivan juga gemar mengecek Facebook tanpa alasan yang jelas (sekadar buka dan lihat-lihat saja), membagi ceritanya di blognya, dan bermain game di laptopnya. Oh iya satu hal lagi, board game sekarang menjadi salah satu kegemarannya, berkat salah satu temannya di KAIST yang terbilang sangat gila terhadap board game.',
     image: 'bivan.jpg',
+  },
+  {
+    name: 'Kevin Christian Wibisono',
+    description: 'Kevin Christian Wibisono ialah seorang pencinta matematika yang diberi kesempatan untuk mendemonstrasikan kegemarannya sekaligus merepresentasikan negaranya dalam ajang olimpiade matematika paling prestisius bagi siswa sekolah dasar dan menengah. Saat ini, Kevin sedang menempuh pendidikan tinggi di National University of Singapore (NUS). Keinginannya untuk mendalami matematika membuatnya memilih jurusan Applied Mathematics. Sebagai bentuk kontribusi kepada pendidikan olimpiade matematika di Indonesia, Kevin mendedikasikan dirinya menjadi Ketua Tim Pengolah KTO Matematika, yang tanggung jawabnya meliputi pemeriksaan kualitas soal, jawaban, dan pembuatan buku. Pada waktu senggang, Kevin suka menonton film kriminal dan mengerjakan soal-soal olimpiade matematika.',
+    image: 'kevin.jpg',
+  },
+  {
+    name: 'Kevin Winata',
+    description: 'Kevin Winata berasal dari Sibolga, sebuah kota di Sumatera Utara yang berpenduduk sekitar 85 ribu orang dan berjarak 350 km dari ibukota provinsinya, Medan. Penghobi kegiatan menggambar ini menyabet medali perak di OSN Matematika 2016, dan sekarang berkuliah di NTU (Nanyang Technological University) di Singapura, jurusan Computer Science. Pemain gitar, competitive programming, dan tenis meja ini merupakan salah satu pemegang amanah dalam pengecekan kualitas soal dan buku KTO Matematika agar sesuai dengan standar, dan turut mendesain beberapa poster KTOM.',
+    image: 'winata.png',
+  },
+  {
+    name: 'Eunice Sukarto',
+    description: 'Eunice merupakan anggota perempuan KTO Matematika kedua setelah Fransisca Andriani (Cis). Kecintaannya terhadap matematika membuahkannya hasil manis berupa medali perunggu di OSN 2015 dan medali perak di OSN 2016. Saat ini, ia sedang melanjutkan studi S1 di UC Berkeley, California dalam bidang ilmu komputer. Peran Eunice di KTO Matematika adalah sebagai desainer, pengisi kuota perempuan (selain Cis) dan membantu mengecek kualitas soal setiap bulannya. Melukis, bermain piano klasik serta jalan-jalan merupakan pelariannya ketika penat belajar matematika. Melihat hasil lukisannya, dapat dikatakan Eunice merupakan pelukis beraliran naturalis, namun ia lebih suka disebut pelukis beraliran manusiawi.',
+    image: 'eunice.png',
   },
 ];
 
