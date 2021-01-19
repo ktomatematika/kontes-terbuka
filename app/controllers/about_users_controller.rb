4c2b9adac1968b2ab82fbe8b774e83e18de37a3b
AudreyFelicio <audreyanwar@gmail.com>
2021-01-20 07:49:41 +0800

    Add ability to edit and update about user

--- app/controllers/about_users_controller.rb
+++ app/controllers/about_users_controller.rb
@@ -5,6 +5,21 @@ class AboutUsersController < ApplicationController
         redirect_to user_path(@user)
     end
 
+    def edit
+        @user = User.find(params[:user_id])
+        @about_user = @user.about_user
+    end
+
+    def update
+        @user = User.find(params[:user_id])
+        
+        if @user.about_user.update(about_user_params)
+            redirect_to user_path(@user)
+        else
+            render 'edit'
+        end
+    end
+
     private 
         def about_user_params
             params.require(:about_user).permit(:name, :description)
