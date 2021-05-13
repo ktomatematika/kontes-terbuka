ef47688f928a069691099c70c1275dbffdcf28b4
AudreyFelicio <audreyanwar@gmail.com>
2021-05-13 16:48:56 +0800

    Address PR review

--- app/models/about_user.rb
+++ app/models/about_user.rb
@@ -1,11 +1,11 @@
 class AboutUser < ActiveRecord::Base
-    # Associations
-    belongs_to :user
-    has_attached_file :image, styles: { small: '150x150>' }
+  # Associations
+  belongs_to :user
+  has_attached_file :image, styles: { small: '150x150>' }
   
-    # Validations
-    validates :name, presence: true
-    validates :description, presence: true
-    validates_attachment :image, presence: true
-    validates_attachment_content_type :image, content_type: ['image/png', 'image/jpeg', 'image/jpg']
+  # Validations
+  validates :name, presence: true
+  validates :description, presence: true
+  validates_attachment :image, presence: true
+  validates_attachment_content_type :image, content_type: ['image/png', 'image/jpeg', 'image/jpg']
 end
