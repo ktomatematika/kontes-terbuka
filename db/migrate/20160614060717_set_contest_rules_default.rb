c75bed852e513098cb72d2d4a1841e742c60a958
nugriz <56295230+nugriz@users.noreply.github.com>
2021-06-21 12:34:10 +0700

    Update 20160614060717_set_contest_rules_default.rb

--- db/migrate/20160614060717_set_contest_rules_default.rb
+++ db/migrate/20160614060717_set_contest_rules_default.rb
@@ -2,7 +2,7 @@
 
 class SetContestRulesDefault < ActiveRecord::Migration
   def change
-    change_column :contests, :rule, :text,
-                  default: File.open('app/assets/default_rules.txt', 'r').read
+    change_column :contests, :rule, :text#,
+                  #default: File.open('app/assets/default_rules.txt', 'r').read
   end
 end
