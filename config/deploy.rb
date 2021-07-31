9e1fe268360cae258e08e9c079c862bfd485e80a
nugriz <56295230+nugriz@users.noreply.github.com>
2021-07-31 11:10:05 +0700

    Remove failed CI & change Capistrano version lock (#142)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid only for current version of Capistrano
-lock '3.11.2'
+lock '3.16.0'
 
 set :application, 'kontes-terbuka'
 set :repo_url, 'git@github.com:donjar/kontes-terbuka.git'
