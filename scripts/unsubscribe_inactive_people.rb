b433b48ab683fa46d47f0dce0d0683d389c978c5
nugriz <rizkinug02@gmail.com>
2021-10-03 11:21:49 +0700

    add script for unsubscribe inactive people with it's test

--- scripts/unsubscribe_inactive_people.rb
+++ scripts/unsubscribe_inactive_people.rb
@@ -1,7 +1,9 @@
-User.all.each do |user|
-    if user.user_contests.order(:created_at).last.created_at < Time.now - 6.month
-        user.user_notifications.each do |user_notification|
-            user_notification.destroy!
+def unsubscribe
+    User.all.each do |user|
+        if user.user_contests.order(:created_at).last.created_at < Time.now - 6.month
+            user.user_notifications.each do |user_notification|
+                user_notification.destroy!
+            end
         end
     end
 end
