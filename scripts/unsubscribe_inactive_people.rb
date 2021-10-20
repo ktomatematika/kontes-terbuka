b4136aa92452b2f2e6c2d12b8f8a33c0e88bc6b2
nugriz <rizkinug02@gmail.com>
2021-10-20 22:17:56 +0700

    fix script to notify users

--- scripts/unsubscribe_inactive_people.rb
+++ scripts/unsubscribe_inactive_people.rb
@@ -5,6 +5,12 @@ def unsubscribe
     next unless user.user_contests.order(:created_at).last.created_at < Time.current - 6.months
 
     user.user_notifications.each(&:destroy!)
-    Mailgun.send_message(to: 'a', text: 'b')
+    Mailgun.send_message(
+      to: user.email,
+      text: "With this email, we apologize for your inconvenience."/
+            "We noticed that you have been inactive from KTOM for more than 6 months."/
+            "For that, KTOM apologize for being forced to unsubscribe you from us since today."/
+            "But you don't have to worry because you can keep join KTOM and subscribe us at our website"/
+            "Once again we apologize and hope you can understand. Thank you.")
   end
 end
