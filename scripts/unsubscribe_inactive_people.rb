bd6502fcab58098c1af7f6652252fb9641bbd481
nugriz <rizkinug02@gmail.com>
2021-12-23 17:06:16 +0700

    return to real time limit that forget to remove

--- scripts/unsubscribe_inactive_people.rb
+++ scripts/unsubscribe_inactive_people.rb
@@ -2,7 +2,7 @@
 
 def unsubscribe
   User.all.each do |user|
-    next unless user.user_contests.order(:created_at).last.created_at < Time.current - 2.seconds
+    next unless user.user_contests.order(:created_at).last.created_at < Time.current - 6.months
 
     user.user_notifications.each(&:destroy!)
     Mailgun.send_message(
