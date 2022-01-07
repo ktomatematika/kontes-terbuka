1e738b8f8e5d3932e2e68767127adc2b62f55af6
nugriz <rizkinug02@gmail.com>
2022-01-08 00:40:01 +0700

    fix undefined path

--- scripts/unsubscribe_inactive_people.rb
+++ scripts/unsubscribe_inactive_people.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 def unsubscribe
+  include Rails.application.routes.url_helpers
   User.all.each do |user|
     next unless user.user_contests.order(:created_at).last.created_at < Time.current - 6.months
 
