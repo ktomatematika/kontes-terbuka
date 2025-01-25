d5a7bf2d987b0c003b0ecc0eaac944d5c0283fb8
Ella Raputri <ellaraputri@gmail.com>
2025-01-25 14:49:34 +0700

    feature: responsive layout

--- app/helpers/about_users_helper.rb
+++ app/helpers/about_users_helper.rb
@@ -19,16 +19,25 @@ module AboutUsersHelper
     users.each_with_index do |data, index|
       base_class = 'about-us-person col-md-4 col-sm-6'
       inner_tags = []
-      inner_tags.append(content_tag(:img, nil, style: 'border-radius: 50%;', src: data.image.url(:small)))
+
+      image_url = data.image.url.present? ? data.image.url : 'assets/panitia/ilhan.jpg'
+      image_filename = File.basename(image_url, ".*") 
+      image_extension = File.extname(image_url)
+      formatted_image_url = "assets/panitia/#{image_filename}#{image_extension}"
+
+      # puts "Formatted Image URL for #{data.name}: #{formatted_image_url}"
+
+      inner_tags.append(content_tag(:img, nil, style: 'border-radius: 50%;', src: formatted_image_url))
       inner_tags.append(content_tag(:h3, data.name.to_s))
       generated_divs.append(content_tag(:div, nil,
                                         class: base_class.to_s,
                                         data: { name: data.name.to_s, description: data.description.to_s }) do
                                           inner_tags.map { |x| concat(x) }
                                         end)
+      generated_divs.append(content_tag(:div, nil, class: 'about-us-description'))
+
       generate_extra_classes(index, generated_divs)
       end_flag = false if index.odd? && index % 3 == 2
-      generated_divs.append(content_tag(:div, nil, class: 'about-us-description'))
     end
     [generated_divs, end_flag]
   end
