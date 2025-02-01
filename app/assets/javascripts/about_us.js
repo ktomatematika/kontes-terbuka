252ae401f1c79dc32fbf654c6084f10c75a664ab
Ella Raputri <ellaraputri@gmail.com>
2025-02-01 11:55:44 +0700

    fix: redundant code comments

--- app/assets/javascripts/about_us.js
+++ app/assets/javascripts/about_us.js
@@ -1,24 +1,3 @@
-// $(document).on('turbolinks:load', function() {
-//   function reset_descriptions() {
-//     $('.about-us-description').slideUp('fast');
-//     $('.active-about-us').removeClass('active-about-us');
-//   }
-//   $('.about-us-description').hide();
-
-//   $('.about-us-person').click(function() {
-//     var me = $(this);
-//     var deactivate = me.hasClass('active-about-us');
-//     reset_descriptions();
-//     if (!deactivate) {
-//       me.addClass('active-about-us');
-//       me.nextAll('.visible-' + window_type() + ':first')
-//         .nextAll('.about-us-description:first')
-//         .text(me.data('description')).slideDown('fast');
-//       ga('send', 'event', 'about-us', 'view', $(this).data('name'));
-//     }
-//   });
-// });
-
 $(document).on('turbolinks:load', function () {
   function reset_descriptions() {
     $('.about-us-description').slideUp('fast'); 
